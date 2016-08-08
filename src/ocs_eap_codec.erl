f421d2f2d55ecc21b74682daf29bb693d93ba70f
Kalpa Gunarathna <kalpa@sigscale.com>
2016-08-08 05:02:15 -0500

    change length value

--- src/ocs_eap_codec.erl
+++ src/ocs_eap_codec.erl
@@ -32,8 +32,8 @@ packet(<<Code, Identifier,_Length:16, Data/binary>> = Packet)
 		when size(Packet) =:= Length ->
 	#eap_packet{code = Code, identifier = Identifier, data = Data};	
 packet(#eap_packet{code = Code, identifier = Identifier,
-		data = Data} = _Packet) when is_integer(Code), is_integer(Identifier),
+		data = Data} ) when is_integer(Code), is_integer(Identifier),
 		is_binary(Data) ->
-	Length = size(Data) + 18,
+	Length = size(Data) + 32,
 	<<Code, Identifier, Length:16, Data>>.
 
