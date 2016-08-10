5a5aeb50e4a174129fa87766f9eb3db74b8995e8
Kalpa Gunarathna <kalpa@sigscale.com>
2016-08-10 01:39:11 -0500

    implement a server supervisor to handle RADIUS auth processes

---
+++ src/ocs_radius_auth_server_sup.erl
@@ -0,0 +1,51 @@
+%%% ocs_radius_auth_server_sup.erl
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @copyright 2016 SigScale Global Inc.
+%%% @end
+%%% Licensed under the Apache License, Version 2.0 (the "License");
+%%% you may not use this file except in compliance with the License.
+%%% You may obtain a copy of the License at
+%%%
+%%%     http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%% Unless required by applicable law or agreed to in writing, software
+%%% distributed under the License is distributed on an "AS IS" BASIS,
+%%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%% See the License for the specific language governing permissions and
+%%% limitations under the License.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @docfile "{@docsrc supervision.edoc}"
+%%%
+-module(ocs_radius_auth_server_sup).
+-copyright('Copyright (c) 2016 SigScale Global Inc.').
+
+-behaviour(supervisor).
+
+%% export the callback needed for supervisor behaviour
+-export([init/1]).
+
+%%----------------------------------------------------------------------
+%%  The supervisor callback
+%%----------------------------------------------------------------------
+
+-spec init(Args :: [term()]) ->
+	{ok, {{supervisor:strategy(), non_neg_integer(), pos_integer()},
+			[supervisor:child_spec()]}} | ignore.
+%% @doc Initialize the {@module} supervisor.
+%% @see //stdlib/supervisor:init/1
+%% @private
+%%
+init(_Args) ->
+	ChildSpecs = [radius_server(radius_server, [])],
+	{ok, {{one_for_one, 10, 60}, ChildSpecs}}.
+
+-spec radius_server(StartMod :: atom(), Args :: [term()]) ->
+	supervisor:child_spec().
+%% @doc Build a supervisor child specification for a
+%% 	{@link //stdlib/gen_server. gen_server} behaviour.
+%% @private
+%%
+radius_server(StartMod, Args) ->
+	StartArgs = [StartMod, Args, []],
+	StartFunc = {gen_server, start_link, StartArgs},
+	{StartMod, StartFunc, permanent, 4000, worker, [StartMod]}.
