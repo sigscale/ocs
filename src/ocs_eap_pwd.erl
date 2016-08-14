b944adffd7e84db7aaaf45c77c453cff10c78ae0
Vance Shipley <vances@motivity.ca>
2016-08-14 21:53:13 +0530

    create and pass random number to nif

--- src/ocs_eap_pwd.erl
+++ src/ocs_eap_pwd.erl
@@ -84,11 +84,11 @@ kdf(_, _, Length, _, _, Res) when size(Res) >= (Length div 8) ->
 compute_pwe(Token, ServerIdentity, PeerIdentity, Password) ->
 	exit(nif_library_not_loaded).
 
--spec compute_scalar() ->
-	{S_rand :: integer(), Scalar_S :: binary(), Element_S :: binary()}.
+-spec compute_scalar(S_rand :: integer()) ->
+	{Scalar_S :: binary(), Element_S :: binary()}.
 %% @doc Compute the Scalar.
 %% 	RFC5931 section 2.8.4.1
-compute_scalar() ->
+compute_scalar(S_rand) ->
 	exit(nif_library_not_loaded).
 
 -spec compute_ks(PWE :: binary(), S_rand :: integer(),
