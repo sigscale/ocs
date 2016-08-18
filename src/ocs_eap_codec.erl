fcecb241ca574e709edf27de29b983e50d15bdd3
Prahveen Thiruchelvam <prahveen@sigscale.com>
2016-08-17 23:08:07 -0500

    implement codec function for eap-pwd header

--- src/ocs_eap_codec.erl
+++ src/ocs_eap_codec.erl
@@ -27,7 +27,7 @@
 -copyright('Copyright (c) 2016 SigScale Global Inc.').
 
 %% export the ocs public API
--export([packet/1]).
+-export([packet/1, pwd/1, payload_id/1]).
 
 -include("ocs_eap_codec.hrl").
 
@@ -43,4 +43,72 @@ packet(#eap_packet{code = Code, identifier = Identifier,
 		is_binary(Data) ->
 	Length = size(Data) + 32,
 	<<Code, Identifier, Length:16, Data/binary>>.
+
+-spec pwd(Packet :: binary()) -> #eap_pwd{}.
+%% @doc Decode an EAP-PWD-Header packet transported in the RADIUS `EAP-Message'
+%% attribute.
+%%
+%% RFC-5931 3.1
+pwd(Packet) ->
+	pwd(Packet, #eap_pwd{}).
+%% @hidden
+pwd(<<Code/binary, T>>, Acc) ->
+	case Code of
+		?Request ->
+			pwd(T, Acc#eap_pwd{code = Code});
+		?Response ->
+			pwd(T, Acc#eap_pwd{code = Code});
+		_ ->
+			pwd(T, Acc)
+	end;
+pwd(<<Identifier/binary, T>>, Acc) ->
+	pwd(T, Acc#eap_pwd{identifier = Identifier});
+pwd(<<Length/binary, T>>, Acc) ->
+	case size(Length) of
+		S when S >= 32 ->
+			pwd(T, Acc#eap_pwd{length = Length});
+		_ ->
+			pwd(T, Acc)
+	end;
+pwd(<<Type/binary, T>>, Acc) ->
+	case Type of
+		?PWD ->
+			pwd(T, Acc#eap_pwd{type = Type});
+		_ ->
+			pwd(T, Acc)
+	end;
+pwd(<<LB/binary, T>>, Acc) ->
+	pwd(T, Acc#eap_pwd{l_bit = LB});
+pwd(<<MB/binary, T>>, Acc) ->
+	pwd(T, Acc#eap_pwd{m_bit = MB});
+pwd(<<PWDExch/binary, T>>, Acc) ->
+	pwd(T, Acc#eap_pwd{pwd_exch = PWDExch});
+pwd(<<TotLength/binary, T>>, Acc) ->
+	case Acc#eap_pwd.l_bit of
+		true ->
+			pwd(T, Acc#eap_pwd{tot_length = TotLength});
+		false ->
+			pwd(T, Acc)
+	end;
+pwd(<<Payload, T>>, Acc) ->
+	pwd(T, Acc#eap_pwd{data = Payload});
+pwd(<<>>, Acc) ->
+	Acc.
+
+-spec pwd(#eap_pwd{}) -> binary().
+%% @doc Encode an eap_pwd record into an EAP-PWD-Header packet transportedi
+%% in the RADIUS `EAP-Message' attribute.
+%%
+%% RFC-5931 3.1
+pwd(#eap_pwd{code = C, identifier = I, length = Len, type = Type,
+	 l_bit = L,
+	 m_bit = M, pwd_exch = P, data = D } = Packet) -> 
+	Length = byte_size(<<C/binary, I/binary, Type/binary, 0:16, D/binary>>),
+	case L of
+		true ->
+			#eap_pwd{tot_length = TLen},
+			<<C, I, Length, _, L, M, P, TLen, D>>;
+		false ->
+			<<C, I, Length, _, L, M, P, D>>
+	end.
 
