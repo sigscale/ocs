bf81ad161aa98b9e19c142b9564c3d130ab22322
Kalpa Gunarathna <kalpa@sigscale.com>
2016-08-23 05:26:11 -0500

    move ECC related definitions to ocs_eap_codec.hrl

--- include/ocs_eap_codec.hrl
+++ include/ocs_eap_codec.hrl
@@ -53,6 +53,15 @@
 -define(TEAP,             55).
 -define(Experimental,    255).
 
+%% Macro definitions for Elliptic Curve Cryptography(ECC) calculations
+
+-define(P,  16#ffffffff00000001000000000000000000000000ffffffffffffffffffffffff).
+-define(A,  16#ffffffff00000001000000000000000000000000fffffffffffffffffffffffc).
+-define(B,  16#5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b).
+-define(Gx, 16#6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296).
+-define(Gy, 16#4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5).
+-define(R,  16#ffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551).
+
 -record(eap_packet,
 			{code :: 1..255,
 			identifier :: 1..255,
