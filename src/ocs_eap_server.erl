551cd2867e46a3362dcab49708714cde238f3d61
Kalpa Gunarathna <kalpa@sigscale.com>
2016-09-05 15:33:00 +0530

    update for radius-1.3 api changes

--- src/ocs_eap_server.erl
+++ src/ocs_eap_server.erl
@@ -178,7 +178,7 @@ access_request(Address, Port, Secret, #radius{id = Identifier,
 				radius_attributes:find(?NasIpAddress, Attributes)} of
 			{{ok, NasId}, _} ->
 				NasId;
-			{error, {ok, NasAddr}} ->
+			{{error, not_found}, {ok, NasAddr}} ->
 				NasAddr
 		end,
 		NasPort = case radius_attributes:find(?NasPort, Attributes) of
