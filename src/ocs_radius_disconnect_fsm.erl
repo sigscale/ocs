f34781769c510bf069f69a6bbce91d0f11b9b46f
Kalpa Gunarathna <kalpa@sigscale.com>
2016-10-07 10:48:32 +0530

    add fsm_sup and fsm for RADIUS disconnect procedure

---
+++ src/ocs_radius_disconnect_fsm.erl
@@ -0,0 +1,134 @@
+%%% ocs_radius_disconnect_fsm.erl
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @copyright 2016 SigScale Global Inc.
+%%% @end
+%%% Licensed under the Apache License, Version 2.0 (the "License");
+%%% you may not use this file except in compliance with the License.
+%%% You may obtain a copy of the License at
+%%%
+%%%     http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%% Unless required by applicable law or agreed to in writing, software
+%%% distributed under the License is distributed on an "AS IS" BASIS,
+%%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%% See the License for the specific language governing permissions and
+%%% limitations under the License.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @reference <a href="http://tools.ietf.org/rfc/rfc3576.txt">
+%%% 	RFC3576 - Dynamic Authorization Extensions for RADIUS</a>
+%%%
+-module(ocs_radius_disconnect_fsm).
+-copyright('Copyright (c) 2016 SigScale Global Inc.').
+
+-behaviour(gen_fsm).
+
+%% export the ocs_radius_disconnect_fsm API
+-export([]).
+
+%% export the ocs_radius_disconnect_fsm state callbacks
+-export([]).
+
+%% export the call backs needed for gen_fsm behaviour
+-export([init/1, handle_event/3, handle_sync_event/4, handle_info/3,
+			terminate/3, code_change/4]).
+
+%% @headerfile "include/radius.hrl"
+-include_lib("radius/include/radius.hrl").
+-include("ocs_eap_codec.hrl").
+
+-define(TIMEOUT, 30000).
+
+%%----------------------------------------------------------------------
+%%  The ocs_radius_disconnect_fsm API
+%%----------------------------------------------------------------------
+
+%%----------------------------------------------------------------------
+%%  The ocs_radius_disconnect_fsm gen_fsm call backs
+%%----------------------------------------------------------------------
+
+-spec init(Args :: list()) ->
+	Result :: {ok, StateName :: atom(), StateData :: #statedata{}}
+		| {ok, StateName :: atom(), StateData :: #statedata{},
+			Timeout :: non_neg_integer() | infinity}
+		| {ok, StateName :: atom(), StateData :: #statedata{}, hibernate}
+		| {stop, Reason :: term()} | ignore.
+%% @doc Initialize the {@module} finite state machine.
+%% @see //stdlib/gen_fsm:init/1
+%% @private
+%%
+init( _Args) ->
+	{stop, not_implemented}.
+
+-spec handle_event(Event :: term(), StateName :: atom(),
+		StateData :: #statedata{}) ->
+	Result :: {next_state, NextStateName :: atom(), NewStateData :: #statedata{}}
+		| {next_state, NextStateName :: atom(), NewStateData :: #statedata{},
+		Timeout :: non_neg_integer() | infinity}
+		| {next_state, NextStateName :: atom(), NewStateData :: #statedata{}, hibernate}
+		| {stop, Reason :: normal | term(), NewStateData :: #statedata{}}.
+%% @doc Handle an event sent with
+%% 	{@link //stdlib/gen_fsm:send_all_state_event/2.
+%% 	gen_fsm:send_all_state_event/2}.
+%% @see //stdlib/gen_fsm:handle_event/3
+%% @private
+%%
+handle_event(_Event, StateName, StateData) ->
+	{next_state, StateName, StateData}.
+
+-spec handle_sync_event(Event :: term(), From :: {Pid :: pid(), Tag :: term()},
+		StateName :: atom(), StateData :: #statedata{}) ->
+	Result :: {reply, Reply :: term(), NextStateName :: atom(), NewStateData :: #statedata{}}
+		| {reply, Reply :: term(), NextStateName :: atom(), NewStateData :: #statedata{},
+		Timeout :: non_neg_integer() | infinity}
+		| {reply, Reply :: term(), NextStateName :: atom(), NewStateData :: #statedata{}, hibernate}
+		| {next_state, NextStateName :: atom(), NewStateData :: #statedata{}}
+		| {next_state, NextStateName :: atom(), NewStateData :: #statedata{},
+		Timeout :: non_neg_integer() | infinity}
+		| {next_state, NextStateName :: atom(), NewStateData :: #statedata{}, hibernate}
+		| {stop, Reason :: normal | term(), Reply :: term(), NewStateData :: #statedata{}}
+		| {stop, Reason :: normal | term(), NewStateData :: #statedata{}}.
+%% @doc Handle an event sent with
+%% 	{@link //stdlib/gen_fsm:sync_send_all_state_event/2.
+%% 	gen_fsm:sync_send_all_state_event/2,3}.
+%% @see //stdlib/gen_fsm:handle_sync_event/4
+%% @private
+%%
+handle_sync_event(_Event, _From, StateName, StateData) ->
+	{reply, ok, StateName, StateData}.
+
+-spec handle_info(Info :: term(), StateName :: atom(), StateData :: #statedata{}) ->
+	Result :: {next_state, NextStateName :: atom(), NewStateData :: #statedata{}}
+		| {next_state, NextStateName :: atom(), NewStateData :: #statedata{},
+		Timeout :: non_neg_integer() | infinity}
+		| {next_state, NextStateName :: atom(), NewStateData :: #statedata{}, hibernate}
+		| {stop, Reason :: normal | term(), NewStateData :: #statedata{}}.
+%% @doc Handle a received message.
+%% @see //stdlib/gen_fsm:handle_info/3
+%% @private
+%%
+handle_info(_Info, StateName, StateData) ->
+	{next_state, StateName, StateData}.
+
+-spec terminate(Reason :: normal | shutdown | term(), StateName :: atom(),
+		StateData :: #statedata{}) -> any().
+%% @doc Cleanup and exit.
+%% @see //stdlib/gen_fsm:terminate/3
+%% @private
+%%
+terminate(_Reason, _StateName, _StateData) ->
+	ok.
+
+-spec code_change(OldVsn :: (Vsn :: term() | {down, Vsn :: term()}),
+		StateName :: atom(), StateData :: #statedata{}, Extra :: term()) ->
+	Result :: {ok, NextStateName :: atom(), NewStateData :: #statedata{}}.
+%% @doc Update internal state data during a release upgrade&#047;downgrade.
+%% @see //stdlib/gen_fsm:code_change/4
+%% @private
+%%
+code_change(_OldVsn, StateName, StateData, _Extra) ->
+	{ok, StateName, StateData}.
+
+%%----------------------------------------------------------------------
+%%  internal functions
+%%----------------------------------------------------------------------
+
