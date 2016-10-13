360870a712e82ad58d1f425bdf673ba0c4b68338
Kalpa Gunarathna <kalpa@sigscale.com>
2016-10-13 17:16:48 +0530

    remove send event

--- src/ocs_acct_server.erl
+++ src/ocs_acct_server.erl
@@ -241,7 +241,6 @@ accounting_request(Address, Port, Secret, Radius,
 						case supervisor:start_child(DiscSup, [[NasIpAddressV, NasIdentifierV,
 								Subscriber, AcctSessionId, Secret], []]) of
 							{ok, Child} ->
-								gen_fsm:send_event(Child, disconnect),
 								{reply, {ok, wait}, State};
 							{error, Reason} ->
 								error_logger:error_report(["Failed to initiate session disconnect function",
