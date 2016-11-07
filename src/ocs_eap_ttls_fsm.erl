43db6ba0aea5016e8ef690f1b6c3810ab02c168d
prahveen <prahveen@sigscale.com>
2016-11-07 18:35:44 +0530

    verify client change cipher

--- src/ocs_eap_ttls_fsm.erl
+++ src/ocs_eap_ttls_fsm.erl
@@ -458,16 +458,21 @@ client_key_exchange({#radius{code = ?AccessRequest, id = RadiusID,
 			{stop, {shutdown, SessionID}, NewStateData}
 	end.
 %% @hidden
-client_key_exchange1(<<?Handshake, _:32, ?ClientKeyExchange, _/binary>>,
-		StateData) ->
-	{next_state, client_key_exchange, StateData, ?TIMEOUT};
-client_key_exchange1(<<?ChangeCipherSpec, _:32, _/binary>> = TtlsRecord,
+client_key_exchange1(<<?Handshake, _:16, Length:16,
+		?ClientKeyExchange, TtlsRecords/binary>>, StateData) ->
+	Size = Length + 5,
+	<<Chunk:Size/binary, Rest/binary>> = TtlsRecords,
+	client_key_exchange1(Rest, StateData);
+client_key_exchange1(<<?ChangeCipherSpec, _:16, Length:16, CCS>>,
 		#statedata{ssl_pid = SslPid} = StateData) ->
-	ocs_eap_ttls_transport:deliver(SslPid, self(), TtlsRecord),
-	{next_state, client_key_exchange, StateData, ?TIMEOUT};
-client_key_exchange1(<<?Handshake, _:32, ?Finished, _/binary>>,
-		StateData) ->
-	{next_state, server_change_cipher_spec, StateData, ?TIMEOUT}.
+	Size = Length + 5,
+	<<Chunk:Size/binary, Rest/binary>> = TtlsRecords,
+	client_key_exchange1(Rest, StateData);
+client_key_exchange1(<<?Handshake, _:16, 1:16, ?Finished, 1>>,
+		#statedata{rx_buf = RxBuf} = StateData) ->
+	ocs_eap_ttls_transport:deliver(SslPid, self(), RxBuf),
+	client_key_exchange1(Rest, StateData),
+	{next_state, server_change_cipher_spec, NextStateData, ?TIMEOUT}.
 
 -spec server_change_cipher_spec(Event :: timeout | term(), StateData :: #statedata{}) ->
 	Result :: {next_state, NextStateName :: atom(), NewStateData :: #statedata{}}
