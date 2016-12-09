c09e89c975e0eb52474cb48a11c51f555c008b8f
prahveen <prahveen@sigscale.com>
2016-12-09 17:37:34 +0530

    remove debug

--- src/ocs_wm_res_subscriber.erl
+++ src/ocs_wm_res_subscriber.erl
@@ -151,12 +151,10 @@ create_path(ReqData, Context) ->
 	Body = wrq:req_body(ReqData),
 	try 
 		{struct, Object} = mochijson:decode(Body),
-erlang:display({obj, Object}),
 		{_, Subscriber} = lists:keyfind("subscriber", 1, Object),
 		{_, Password} = lists:keyfind("password", 1, Object),
 		{_, {struct, AttrJs}} = lists:keyfind("attributes", 1, Object),
 		Attributes = sub_attributes(AttrJs),
-erlang:display({att, Attributes}),
 		{_, BalStr} = lists:keyfind("balance", 1, Object),
 		{Balance , _}= string:to_integer(BalStr),
 		NewContext = Context#state{subscriber = Subscriber,
