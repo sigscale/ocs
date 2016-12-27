48dbd5d71565519dbd9972db3321f8bb2342c006
Kalpa Gunarathna <kalpa@sigscale.com>
2016-12-27 15:42:01 +0530

    add test suite for REST api

---
+++ test/ocs_rest_api_SUITE.erl
@@ -0,0 +1,95 @@
+%%% ocs_rest_api_SUITE.erl
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @copyright 2016 SigScale Global Inc.
+%%% @end
+%%% Licensed under the Apache License, Version 2.0 (the "License");
+%%% you may not use this file except in compliance with the License.
+%%% You may obtain a copy of the License at
+%%%
+%%%     http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%% Unless required by applicable law or agreed to in writing, software
+%%% distributed under the License is distributed on an "AS IS" BASIS,
+%%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%% See the License for the specific language governing permissions and
+%%% limitations under the License.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%  Test suite for the ocs API.
+%%%
+-module(ocs_rest_api_SUITE).
+-copyright('Copyright (c) 2016 SigScale Global Inc.').
+
+%% common_test required callbacks
+-export([suite/0, sequences/0, all/0]).
+-export([init_per_suite/1, end_per_suite/1]).
+-export([init_per_testcase/2, end_per_testcase/2]).
+
+-compile(export_all).
+
+%% @headerfile "include/radius.hrl"
+-include_lib("radius/include/radius.hrl").
+-include("ocs_eap_codec.hrl").
+-include_lib("common_test/include/ct.hrl").
+
+%%---------------------------------------------------------------------
+%%  Test server callback functions
+%%---------------------------------------------------------------------
+
+-spec suite() -> DefaultData :: [tuple()].
+%% Require variables and set default values for the suite.
+%%
+suite() ->
+	[{userdata, [{doc, "This suite tests the application's REST API."}]}, 
+	{timetrap, {minutes, 1}}].
+
+-spec init_per_suite(Config :: [tuple()]) -> Config :: [tuple()].
+%% Initialization before the whole suite.
+%%
+init_per_suite(Config) ->
+	ok = ocs_test_lib:initialize_db(),
+	ok = ocs_test_lib:start(),
+	Config.
+
+-spec end_per_suite(Config :: [tuple()]) -> any().
+%% Cleanup after the whole suite.
+%%
+end_per_suite(Config) ->
+	ok = ocs_test_lib:stop(),
+	Config.
+
+-spec init_per_testcase(TestCase :: atom(), Config :: [tuple()]) -> Config :: [tuple()].
+%% Initialization before each test case.
+%%
+init_per_testcase(_TestCase, Config) ->
+	{ok, IP} = application:get_env(ocs, radius_auth_addr),
+	{ok, Socket} = gen_udp:open(0, [{active, false}, inet, {ip, IP}, binary]),
+	[{socket, Socket} | Config].
+
+-spec end_per_testcase(TestCase :: atom(), Config :: [tuple()]) -> any().
+%% Cleanup after each test case.
+%%
+end_per_testcase(_TestCase, Config) ->
+	Socket = ?config(socket, Config),
+	ok =  gen_udp:close(Socket).
+
+-spec sequences() -> Sequences :: [{SeqName :: atom(), Testcases :: [atom()]}].
+%% Group test cases into a test sequence.
+%%
+sequences() -> 
+	[].
+
+-spec all() -> TestCases :: [Case :: atom()].
+%% Returns a list of all test cases in this test suite.
+%%
+all() -> 
+	[].
+
+%%---------------------------------------------------------------------
+%%  Test cases
+%%---------------------------------------------------------------------
+
+
+%%---------------------------------------------------------------------
+%%  Internal functions
+%%---------------------------------------------------------------------
+
--- test/test.spec
+++ test/test.spec
@@ -1,3 +1,3 @@
 {suites, ".", [ocs_eap_ttls_SUITE, ocs_eap_SUITE, ocs_api_SUITE,
-					ocs_accounting_SUITE, ocs_codec_eap_SUITE,
+					ocs_rest_api_SUITE, ocs_accounting_SUITE, ocs_codec_eap_SUITE,
 					ocs_simple_auth_SUITE]}.
