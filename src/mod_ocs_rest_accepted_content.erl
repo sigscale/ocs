0f641557a572b6e423c6d112bc1edf6c95162dd6
Kalpa Gunarathna <kalpa@sigscale.com>
2017-01-19 17:48:06 +0530

    DELETE request does not include content-type

--- src/mod_ocs_rest_accepted_content.erl
+++ src/mod_ocs_rest_accepted_content.erl
@@ -45,23 +45,23 @@
 	Fun :: fun((Arg) -> sent| close | Body),
 	Arg :: [term()].
 %% @doc Erlang web server API callback function.
-do(#mod{parsed_header = Headers, request_uri = Uri,
+do(#mod{method = Method, parsed_header = Headers, request_uri = Uri,
 				data = Data} = _ModData) ->
 	case string:tokens(Uri, "/") of
 		["ocs", "v1", Resource] ->
-			do_accept(Headers, Resource, Data);
+			do_accept(Headers, Method, Resource, Data);
 		["ocs", "v1", Resource, _Id] ->
-			do_accept(Headers, Resource, Data);
+			do_accept(Headers, Method, Resource, Data);
 		_ ->
 			Response = "<h2>HTTP Error 400 - Bad Request</h2>",
 			{break, [{response, {400, Response}}]}
 	end.
 
 %% @hidden
-do_accept(Headers, Resource, Data) ->
+do_accept(Headers, Method, Resource, Data) ->
+	ResourceName = list_to_atom("ocs_rest_res_" ++ Resource),
 	case lists:keyfind("content-type", 1, Headers) of
 		{_, ProvidedType} ->
-			ResourceName = list_to_atom("ocs_rest_res_" ++ Resource),
 			AcceptedTypes = ResourceName:content_types_accepted(),
 			case lists:member(ProvidedType, AcceptedTypes) of
 				true ->
@@ -70,6 +70,8 @@ do_accept(Headers, Resource, Data) ->
 					Response = "<h2>HTTP Error 415 - Unsupported Media Type</h2>",
 					{break, [{response, {415, Response}}]}
 			end;
+		_ when Method == "DELETE" ->
+			{proceed, [{resource, ResourceName} | Data]};
 		_ ->
 			Response = "<h2>HTTP Error 400 - Bad Request</h2>",
 			{break, [{response, {400, Response}}]}
