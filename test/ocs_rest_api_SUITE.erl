fdc347612b14cc5c6136a8bd5280dbd3e44702ad
Krishman Logenderarajah <krishmanl@sigscale.com>
2017-02-16 16:20:10 +0530

    implement delete_subscriber testcase for rest interface

--- test/ocs_rest_api_SUITE.erl
+++ test/ocs_rest_api_SUITE.erl
@@ -122,7 +122,7 @@ sequences() ->
 %% Returns a list of all test cases in this test suite.
 %%
 all() -> 
-	[add_subscriber, get_subscriber, retrieve_all_subscriber].
+	[add_subscriber, get_subscriber, retrieve_all_subscriber, delete_subscriber].
 
 %%---------------------------------------------------------------------
 %%  Test cases
@@ -278,6 +278,44 @@ retrieve_all_subscriber(Config) ->
 	SortedAttributes = lists:sort(Attributes -- ExtraAttributes),
 	{"balance", Balance} = lists:keyfind("balance", 1, Subscriber),
 	{"enabled", Enable} = lists:keyfind("enabled", 1, Subscriber).
+
+delete_subscriber() ->
+   [{userdata, [{doc,"Delete subscriber in rest interface"}]}].
+
+delete_subscriber(Config) ->
+	ContentType = "application/json",
+   ID = "eacfd73ae11d",
+   Password = "ksc8c333npqc",
+   AsendDataRate = {struct, [{"name", "ascendDataRate"}, {"type", 26},
+      {"vendorId", 529}, {"vendorType", 197}, {"value", 1024}]},
+   AsendXmitRate = {struct, [{"name", "ascendXmitRate"}, {"type", 26},
+      {"vendorId", 529}, {"vendorType", 255}, {"value", 512}]},
+   SessionTimeout = {struct, [{"name", "sessionTimeout"}, {"value", 10864}]},
+   Interval = {struct, [{"name", "acctInterimInterval"}, {"value", 300}]},
+   Class = {struct, [{"name", "class"}, {"value", "skiorgs"}]},
+   SortedAttributes = lists:sort([AsendDataRate, AsendXmitRate, SessionTimeout, Interval, Class]),
+   AttributeArray = {array, SortedAttributes},
+   Balance = 100,
+   Enable = true,
+   JSON1 = {struct, [{"id", ID}, {"password", Password},
+   {"attributes", AttributeArray}, {"balance", Balance}, {"enabled", Enable}]},
+   RequestBody = lists:flatten(mochijson:encode(JSON1)),
+   HostUrl = ?config(host_url, Config),
+   Accept = {"accept", "application/json"},
+	RestUser = ct:get_config(rest_user),
+   RestPass = ct:get_config(rest_pass),
+   Encodekey = base64:encode_to_string(string:concat(RestUser ++ ":", RestPass)),
+   AuthKey = "Basic " ++ Encodekey,
+   Authentication = {"authorization", AuthKey},
+   Request1 = {HostUrl ++ "/ocs/v1/subscriber", [Accept, Authentication], ContentType, RequestBody},
+   {ok, Result} = httpc:request(post, Request1, [], []),
+   {{"HTTP/1.1", 201, _Created}, Headers, _} = Result,
+   {_, URI1} = lists:keyfind("location", 1, Headers),
+   {_, _, URI2, _, _} = mochiweb_util:urlsplit(URI1),
+   Request2 = {HostUrl ++ URI2, [Accept, Authentication], ContentType, []},
+   {ok, Result1} = httpc:request(delete, Request2, [], []),
+   {{"HTTP/1.1", 204, _NoContent}, Headers1, []} = Result1,
+	{_, "0"} = lists:keyfind("content-length", 1, Headers1).
 
 %%---------------------------------------------------------------------
 %%  Internal functions
