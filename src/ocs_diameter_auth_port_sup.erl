faff4eab0672f5b86bff0ae447c44c39722d9dd1
Kalpa Gunarathna <kalpa@sigscale.com>
2017-04-24 13:35:55 +0530

    start ocs_simple_auth_fsm_sup

--- src/ocs_diameter_auth_port_sup.erl
+++ src/ocs_diameter_auth_port_sup.erl
@@ -41,6 +41,7 @@
 %%
 init([Address, Port, _LogRotateTime, Options]) ->
 	ChildSpecs = [server(ocs_diameter_auth_port_server, Address, Port, Options),
+		supervisor(ocs_simple_auth_fsm_sup, []),
 		supervisor(ocs_diameter_auth_service_fsm_sup, [Address, Port])],
 	{ok, {{one_for_one, 10, 3600}, ChildSpecs}}.
 
