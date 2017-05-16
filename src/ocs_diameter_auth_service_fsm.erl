fa93535c5c976b21370a11b198aab05347884e5e
Kalpa Gunarathna <kalpa@sigscale.com>
2017-05-16 11:54:56 +0530

    remove cc application options from diameter auth service

--- src/ocs_diameter_auth_service_fsm.erl
+++ src/ocs_diameter_auth_service_fsm.erl
@@ -44,11 +44,11 @@
 
 -define(DIAMETER_SERVICE, ocs_diameter_service).
 -define(BASE_APPLICATION, ocs_diameter_base_application).
--define(NAS_APPLICATION, ocs_diameter_nas_application).
--define(CC_APPLICATION, ocs_diameter_cc_application).
+-define(BASE_APPLICATION_ID, 0).
 -define(BASE_APPLICATION_CALLBACK, ocs_diameter_base_application_cb).
+-define(NAS_APPLICATION, ocs_diameter_nas_application).
+-define(NAS_APPLICATION_ID, 1).
 -define(NAS_APPLICATION_CALLBACK, ocs_diameter_nas_application_cb).
--define(CC_APPLICATION_CALLBACK, ocs_diameter_cc_application_cb).
 
 %%----------------------------------------------------------------------
 %%  The ocs_diameter_auth_service_fsm API
@@ -268,7 +268,7 @@ service_options() ->
 		{'Origin-Realm', "sigscale.com"},
 		{'Vendor-Id', 0},
 		{'Product-Name', "SigScale Diameter Server"},
-		{'Auth-Application-Id', [0, 1, 4]},
+		{'Auth-Application-Id', [0, 1]},
 		{restrict_connections, false},
 		{string_decode, false},
 		{application, [{alias, ?BASE_APPLICATION},
@@ -276,10 +276,7 @@ service_options() ->
 				{module, ?BASE_APPLICATION_CALLBACK}]},
 		{application, [{alias, ?NAS_APPLICATION},
 				{dictionary, diameter_gen_nas_application_rfc7155},
-				{module, ?NAS_APPLICATION_CALLBACK}]},
-		{application, [{alias, ?CC_APPLICATION},
-				{dictionary, diameter_gen_cc_application_rfc4006},
-				{module, ?CC_APPLICATION_CALLBACK}]}].
+				{module, ?NAS_APPLICATION_CALLBACK}]}].
 
 -spec transport_options(Transport, Address, Port) -> Options
 	when
