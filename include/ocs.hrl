7c1b874339ea9d8a78db4895612894690e022370
Kalpa Gunarathna <kalpa@sigscale.com>
2017-07-25 14:27:10 +0530

    add session related attributes to subscriber

--- include/ocs.hrl
+++ include/ocs.hrl
@@ -34,6 +34,8 @@
 		balance :: integer(),
 		enabled = true :: boolean(),
 		disconnect  = false :: boolean(),
+		session_attributes :: radius_attributes:attributes(),
+		allow_multi_session  = false :: boolean(),
 		last_modified  = {erlang:system_time(milli_seconds),
 				erlang:unique_integer([positive])} :: tuple()}).
 
