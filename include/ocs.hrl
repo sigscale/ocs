d1964939857499590c2f03867eea5aa6de8d9832
Kalpa Gunarathna <kalpa@sigscale.com>
2017-07-26 16:54:20 +0530

    a subcriber can have multiple sessions

--- include/ocs.hrl
+++ include/ocs.hrl
@@ -34,7 +34,7 @@
 		balance :: integer(),
 		enabled = true :: boolean(),
 		disconnect  = false :: boolean(),
-		session_attributes = radius_attributes:new() :: radius_attributes:attributes(),
+		session_attributes = radius_attributes:new() :: [radius_attributes:attributes()],
 		multi_sessions_allowed  = false :: boolean(),
 		last_modified  = {erlang:system_time(milli_seconds),
 				erlang:unique_integer([positive])} :: tuple()}).
