c817680bb7df64cd6a17b5498f0a212f9dfe210f
Vance Shipley <vances@sigscale.org>
2017-07-26 09:36:32 +0530

    add library module for utility functions used by rest servers

---
+++ test/ocs_rest_lib_SUITE.erl
@@ -0,0 +1,85 @@
+%%% ocs_rest_lib_SUITE.erl
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @copyright 2016 - 2017 SigScale Global Inc.
+%%% @end
+%%% Licensed under the Apache License, Version 2.0 (the "License");
+%%% you may not use this file except in compliance with the License.
+%%% You may obtain a copy of the License at
+%%%
+%%%     http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%% Unless required by applicable law or agreed to in writing, software
+%%% distributed under the License is distributed on an "AS IS" BASIS,
+%%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%% See the License for the specific language governing permissions and
+%%% limitations under the License.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%  @doc Test suite for REST utility library
+%%% 	of the {@link //ocs. ocs} application.
+%%%
+-module(ocs_rest_lib_SUITE).
+-copyright('Copyright (c) 2016 - 2017 SigScale Global Inc.').
+
+%% common_test required callbacks
+-export([suite/0, sequences/0, all/0]).
+-export([init_per_suite/1, end_per_suite/1]).
+-export([init_per_testcase/2, end_per_testcase/2]).
+
+%% Note: This directive should only be used in test suites.
+-compile(export_all).
+
+%%---------------------------------------------------------------------
+%%  Test server callback functions
+%%---------------------------------------------------------------------
+
+-spec suite() -> DefaultData :: [tuple()].
+%% Require variables and set default values for the suite.
+%%
+suite() ->
+	[{userdata, [{doc, "Test suite for REST utilities in OCS"}]},
+	{timetrap, {minutes, 1}}].
+
+-spec init_per_suite(Config :: [tuple()]) -> Config :: [tuple()].
+%% Initialization before the whole suite.
+%%
+init_per_suite(Config) ->
+	Config.
+
+-spec end_per_suite(Config :: [tuple()]) -> any().
+%% Cleanup after the whole suite.
+%%
+end_per_suite(Config) ->
+	Config.
+
+-spec init_per_testcase(TestCase :: atom(), Config :: [tuple()]) -> Config :: [tuple()].
+%% Initialization before each test case.
+%%
+init_per_testcase(_TestCase, Config) ->
+	Config.
+
+-spec end_per_testcase(TestCase :: atom(), Config :: [tuple()]) -> any().
+%% Cleanup after each test case.
+%%
+end_per_testcase(_TestCase, _Config) ->
+	ok.
+
+-spec sequences() -> Sequences :: [{SeqName :: atom(), Testcases :: [atom()]}].
+%% Group test cases into a test sequence.
+%%
+sequences() ->
+	[].
+
+-spec all() -> TestCases :: [Case :: atom()].
+%% Returns a list of all test cases in this test suite.
+%%
+all() ->
+	[].
+
+%%---------------------------------------------------------------------
+%%  Test cases
+%%---------------------------------------------------------------------
+
+%%---------------------------------------------------------------------
+%%  Internal functions
+%%---------------------------------------------------------------------
+
