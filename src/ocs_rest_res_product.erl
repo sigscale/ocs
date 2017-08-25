dfb1c9dac0066acb1b6f1c01e5c12033a937ed64
Prahveen Thiruchelvam <prahveen@sigscale.org>
2017-08-25 13:55:57 +0530

    add product resource

---
+++ src/ocs_rest_res_product.erl
@@ -0,0 +1,53 @@
+%%% ocs_rest_res_balance.erl
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @copyright 2016 - 2017 SigScale Global Inc.
+%%% @end
+%%% Licensed under the Apache License, Version 2.0 (the "License");
+%%% you may not use this file except in compliance with the License.
+%%% You may obtain a copy of the License at
+%%%
+%%%     http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%% Unless required by applicable law or agreed to in writing, software
+%%% distributed under the License is distributed on an "AS IS" BASIS,
+%%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%% See the License for the specific language governing permissions and
+%%% limitations under the License.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @doc This library module implements resource handling functions
+%%% 	for a REST server in the {@link //ocs. ocs} application.
+%%%
+-module(ocs_rest_res_product).
+-copyright('Copyright (c) 2016 - 2017 SigScale Global Inc.').
+
+-export([content_types_accepted/0, content_types_provided/0]).
+
+-export([add_product/1]).
+
+-include_lib("radius/include/radius.hrl").
+-include("ocs.hrl").
+
+-spec content_types_accepted() -> ContentTypes
+	when
+		ContentTypes :: list().
+%% @doc Provides list of resource representations accepted.
+content_types_accepted() ->
+	["application/json"].
+
+-spec content_types_provided() -> ContentTypes
+	when
+		ContentTypes :: list().
+%% @doc Provides list of resource representations available.
+content_types_provided() ->
+	["application/json"].
+
+-spec add_product(ReqData) -> Result when
+	ReqData	:: [tuple()],
+	Result	:: {ok, Headers, Body} | {error, Status},
+	Headers	:: [tuple()],
+	Body		:: iolist(),
+	Status	:: 400 | 500 .
+%% @doc Respond to `POST /catalogManagement/v1/product' and
+%% add a new `product'
+add_product(ReqData) ->
+	todo.
