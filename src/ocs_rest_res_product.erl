0c81f185004d662cd20c7c064e48a68adf91f86e
Prahveen Thiruchelvam <prahveen@sigscale.org>
2017-08-28 17:12:54 +0530

    convert ISO time to millisecond

--- src/ocs_rest_res_product.erl
+++ src/ocs_rest_res_product.erl
@@ -27,6 +27,13 @@
 -include_lib("radius/include/radius.hrl").
 -include("ocs.hrl").
 
+%% support deprecated_time_unit()
+-define(MILLISECOND, milli_seconds).
+%-define(MILLISECOND, millisecond).
+
+% calendar:datetime_to_gregorian_seconds({{1970,1,1},{0,0,0}})
+-define(EPOCH, 62167219200).
+
 -spec content_types_accepted() -> ContentTypes
 	when
 		ContentTypes :: list().
@@ -176,14 +183,17 @@ price_type("one_time") ->
 price_type("usage") ->
 	usage.
 
--spec date(MilliSeconds) -> Result
+-spec date(DateTimeFormat) -> Result
 	when
-		MilliSeconds :: pos_integer(),
+		DateTimeFormat :: pos_integer() | tuple(),
 		Result :: calendar:datetime().
-%% @doc Convert timestamp to date and time.
+%% @doc Convert timestamp to date and time or
+%%	date and time to timeStamp.
 date(MilliSeconds) when is_integer(MilliSeconds) ->
 	Seconds = ?EPOCH + (MilliSeconds div 1000),
-	calendar:gregorian_seconds_to_datetime(Seconds).
+	calendar:gregorian_seconds_to_datetime(Seconds);
+date(DateTime) when is_tuple(DateTime) ->
+	calendar:datetime_to_gregorian_seconds(DateTime) - ?EPOCH.
 
 -spec iso8601(MilliSeconds) -> Result
 	when
@@ -198,3 +208,19 @@ iso8601(MilliSeconds) when is_integer(MilliSeconds) ->
 			[Year, Month, Day, Hour, Minute, Second, MilliSeconds rem 1000]),
 	lists:flatten(Chars).
 
+-spec timestamp(ISO8610) -> Result
+	when
+		ISO8610	:: string(),
+		Result	:: pos_integer() | {error, Reason},
+		Reason	:: term().
+%% @doc Convert ISO 8601 format date and time to timestamp.
+timestamp(ISO8610) when is_list(ISO8610) ->
+	DateFormat = "~4d-~2d-~2d",
+	TimeFormat = "T~2d:~2d:~2d.~3dZ",
+	case io_lib:fread(DateFormat ++ TimeFormat, ISO8610) of
+		{ok, [Y, M, D, H, Min, S, MSR], _} ->
+			DateTime = {{Y, M, D}, {H, Min, S + MSR}}
+			date(DateTime);
+		{error, Reason} ->
+			{error, Reason}
+	end.
