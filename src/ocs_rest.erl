a0f12fada4ac01e128e9f4b4c300cb4f708a1b49
Vance Shipley <vances@sigscale.org>
2017-08-29 14:00:51 +0530

    add utility function to parse uri query portion

--- src/ocs_rest.erl
+++ src/ocs_rest.erl
@@ -20,12 +20,37 @@
 -module(ocs_rest).
 -copyright('Copyright (c) 2016 - 2017 SigScale Global Inc.').
 
--export([filter/2, range/1]).
+-export([parse_query/1, filter/2, range/1]).
 
 %%----------------------------------------------------------------------
 %%  The ocs_rest public API
 %%----------------------------------------------------------------------
 
+-spec parse_query(Query) -> Result
+	when
+		Query :: string(),
+		Result :: [{Key, Value}],
+		Key :: string(),
+		Value :: string().
+%% @doc Parse the query portion of a URI.
+%% @throws {error, 400}
+parse_query("?" ++ Query) ->
+	parse_query(Query);
+parse_query(Query) when is_list(Query) ->
+	parse_query(string:tokens(Query, "&"), []).
+%% @hidden
+parse_query([H | T], Acc) ->
+	parse_query(T, parse_query1(H, string:chr(H, $=), Acc));
+parse_query([], Acc) ->
+	lists:reverse(Acc).
+%% @hidden
+parse_query1(_Field, 0, _Acc) ->
+	throw({error, 400});
+parse_query1(Field, N, Acc) ->
+	Key = lists:sublist(Field, N - 1),
+	Value = lists:sublist(Field, N + 1, length(Field)),
+	[{http_uri:decode(Key), http_uri:decode(Value)} | Acc].
+	
 -spec filter(Filters, JsonObject) -> Result
 	when
 		Filters :: [string()],
