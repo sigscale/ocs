a23da3e68358c55d2cd98e242ce9c5525cdfebe3
Prahveen Thiruchelvam <prahveen@sigscale.org>
2017-09-06 18:53:49 +0530

    modifi iso8601 function to convert iso time to timestamp

--- src/ocs_rest.erl
+++ src/ocs_rest.erl
@@ -116,20 +116,69 @@ date(MilliSeconds) when is_integer(MilliSeconds) ->
 	Seconds = ?EPOCH + (MilliSeconds div 1000),
 	calendar:gregorian_seconds_to_datetime(Seconds);
 date(DateTime) when is_tuple(DateTime) ->
-	calendar:datetime_to_gregorian_seconds(DateTime) - ?EPOCH.
+	Seconds = calendar:datetime_to_gregorian_seconds(DateTime) - ?EPOCH,
+	Seconds * 1000.
 
 -spec iso8601(MilliSeconds) -> Result
 	when
-		MilliSeconds	:: pos_integer(),
-		Result			:: string().
-%% @doc Convert timestamp to ISO 8601 format date and time.
+		MilliSeconds	:: pos_integer() | string(),
+		Result			:: string() | pos_integer().
+%% @doc Convert iso8610 to ISO 8601 format date and time.
 iso8601(MilliSeconds) when is_integer(MilliSeconds) ->
 	{{Year, Month, Day}, {Hour, Minute, Second}} = date(MilliSeconds),
 	DateFormat = "~4.10.0b-~2.10.0b-~2.10.0b",
 	TimeFormat = "T~2.10.0b:~2.10.0b:~2.10.0b.~3.10.0bZ",
 	Chars = io_lib:fwrite(DateFormat ++ TimeFormat,
 			[Year, Month, Day, Hour, Minute, Second, MilliSeconds rem 1000]),
-	lists:flatten(Chars).
+	lists:flatten(Chars);
+iso8601(ISODateTime) when is_list(ISODateTime) ->
+	case string:rchr(ISODateTime, $T) of
+		0 ->
+			iso8601(ISODateTime, []);
+		N ->
+			iso8601(lists:sublist(ISODateTime, N - 1),
+				lists:sublist(ISODateTime,  N + 1, length(ISODateTime)))
+	end.
+%% @hidden
+iso8601(Date, Time) when is_list(Date), is_list(Time) ->
+	D = iso8601_date(string:tokens(Date, ",-"), []),
+	{H, Mi, S, Ms} = iso8601_time(string:tokens(Time, ":."), []),
+	date({D, {H, Mi, S}}) + Ms.
+%% @hidden
+iso8601_date([[Y1, Y2, Y3, Y4] | T], _Acc) ->
+	Y = list_to_integer([Y1, Y2, Y3, Y4]),
+	iso8601_date(T, Y);
+iso8601_date([[M1, M2] | T], Y) when is_integer(Y) ->
+	M = list_to_integer([M1, M2]),
+	iso8601_date(T, {Y, M});
+iso8601_date([[D1, D2] | T], {Y, M}) ->
+	D = list_to_integer([D1, D2]),
+	iso8601_date(T, {Y, M, D});
+iso8601_date([], {Y, M}) ->
+	{Y, M, 1};
+iso8601_date([], {Y, M, D}) ->
+	{Y, M, D}.
+%% @hidden
+iso8601_time([H1 | T], []) ->
+	H = list_to_integer(H1),
+	iso8601_time(T, H);
+iso8601_time([M1 | T], H) when is_integer(H) ->
+	Mi = list_to_integer(M1),
+	iso8601_time(T, {H, Mi});
+iso8601_time([S1 | T], {H, Mi}) ->
+	S = list_to_integer(S1),
+	iso8601_time(T, {H, Mi, S});
+iso8601_time([], {H, Mi}) ->
+	{H, Mi, 0, 0};
+iso8601_time([Ms1 | T], {H, Mi, S}) ->
+	Ms = list_to_integer(Ms1),
+	iso8601_time(T, {H, Mi, S, Ms});
+iso8601_time([], {H, Mi, S}) ->
+	{H, Mi, S, 0};
+iso8601_time([], {H, Mi, S, Ms}) ->
+	{H, Mi, S, Ms};
+iso8601_time([], []) ->
+	{0,0,0,0}.
 
 -spec timestamp(ISO8610) -> Result
 	when
