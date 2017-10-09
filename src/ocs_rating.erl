7056548db763ef3fcd5121e1363224daa961d181
Prahveen Thiruchelvam <prahveen@sigscale.org>
2017-10-09 16:28:49 +0530

    validity period for product instance

--- src/ocs_rating.erl
+++ src/ocs_rating.erl
@@ -25,6 +25,10 @@
 
 -include("ocs.hrl").
 
+%% support deprecated_time_unit()
+-define(MILLISECOND, milli_seconds).
+%-define(MILLISECOND, millisecond).
+
 -spec rating(RequestType, SubscriptionRef) -> Return
 	when
 		RequestType :: price_request | tariff_request,
@@ -42,36 +46,63 @@ rating(tariff_request, #subscriber{buckets = Buckets,
 
 	rating1(lists:any(F, Buckets), ProdInst, Buckets, SubscriptionRef).
 %% @hidden
-rating1(false, #product_instance{product = ProdID}, Buckets, SubscriptionRef) ->
-	rating2(ocs:find_product(ProdID), Buckets, SubscriptionRef);
-rating1(true, _ProdInst, _Buckets, SubscriptionRef) ->
-	{ok, SubscriptionRef}.
+rating1(false, #product_instance{product = ProdID, termination_date = EDT}, Buckets, SubscriptionRef) ->
+	rating2(ocs:find_product(ProdID), EDT, Buckets, SubscriptionRef);
+rating1(true, #product_instance{termination_date =
+		EDT, product = ProdID}, Buckets, SubscriptionRef) ->
+	Now = erlang:system_time(?MILLISECOND),
+	if
+		EDT > Now ->
+			{ok, SubscriptionRef};
+		true ->
+			rating2(ocs:find_product(ProdID), EDT, Buckets, SubscriptionRef)
+	end.
 %% @hidden
-rating2({ok, #product{price = Prices}}, Buckets, SubscriptionRef) ->
-	rating3(Prices, Buckets, SubscriptionRef);
-rating2({error, Reason}, _Buckets, _SubscriptionRef) ->
+rating2({ok, #product{price = Prices}}, EDT, Buckets, SubscriptionRef) ->
+	rating3(Prices, EDT, Buckets, SubscriptionRef);
+rating2({error, Reason}, _EDT, _Buckets, _SubscriptionRef) ->
 	{error, Reason}.
 %% @hidden
 rating3([#price{type = recurring, units = cents,
-		amount = Amount, alteration = #alteration{type = recurring, units = octets,
-		size = Size}} | T], Buckets, SubscriptionRef) ->
-	rating4(T, Amount, Size, Buckets, SubscriptionRef);
+		amount = Amount, alteration = #alteration{type = recurring,
+		units = octets, size = Size}} | T], EDT, Buckets, SubscriptionRef) ->
+	Now = erlang:system_time(?MILLISECOND),
+	case EDT > Now of
+		true ->
+			rating4(T, EDT, Amount, Size, Buckets, SubscriptionRef);
+		false ->
+			rating3(T, EDT, Buckets, SubscriptionRef)
+	end;
 rating3([#price{type = usage, size = Size, units = octets,
-		amount = Amount} | T], Buckets, SubscriptionRef) ->
-	rating4(T, Amount, Size, Buckets, SubscriptionRef);
-rating3([], _, _SubscriptionRef) ->
+		amount = Amount} | T], EDT, Buckets, SubscriptionRef) ->
+	rating4(T, EDT, Amount, Size, Buckets, SubscriptionRef);
+rating3([], _, _, _SubscriptionRef) ->
 	{error, rating_failed}.
 %% @hidden
-rating4(Prices, Amount, Size, Buckets, SubscriptionRef) ->
+rating4(Prices, EndTime, Amount, Size, Buckets, #subscriber{product = ProdInst} = SubscriptionRef) ->
 	case lists:keytake(cents, #bucket.bucket_type, Buckets) of
 		{value, #bucket{remain_amount = 
 				#remain_amount{amount = Cents}} = RecuBucket, ReBuckets} when Cents >= Amount ->
 			B1 = #bucket{bucket_type = octets, remain_amount = #remain_amount{amount = Size}},
 			B2 = RecuBucket#bucket{remain_amount = #remain_amount{amount = Cents - Amount}},
 			NewBuckets = [B1, B2 | ReBuckets],
-			{ok, SubscriptionRef#subscriber{buckets = NewBuckets}};
+			SDT = get_std(),
+			EDT = get_edt(monthly, SDT),
+			NewProdInst = ProdInst#product_instance{start_date = SDT, termination_date = EDT},
+			NewSubscriptionRef = SubscriptionRef#subscriber{product = NewProdInst, buckets = NewBuckets},
+			{ok, NewSubscriptionRef};
 		{value, _, _} ->
-			rating3(Prices, Buckets, SubscriptionRef);
+			rating3(Prices, EndTime, Buckets, SubscriptionRef);
 		false ->
 			{error, rating_failed}
 	end.
+
+%%----------------------------------------------------------------------
+%%  internal functions
+%%----------------------------------------------------------------------
+get_std() ->
+	erlang:system_time(?MILLISECOND).
+
+get_edt(monthly, SDT) ->
+	SDT + 2592000. % 3600*24
+
