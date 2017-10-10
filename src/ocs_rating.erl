eba6e74a9c391a7b700a5b2b839fde6c7e58a864
Prahveen Thiruchelvam <prahveen@sigscale.org>
2017-10-10 16:15:21 +0530

    refactor rating function

--- src/ocs_rating.erl
+++ src/ocs_rating.erl
@@ -21,55 +21,134 @@
 -module(ocs_rating).
 -copyright('Copyright (c) 2016 - 2017 SigScale Global Inc.').
 
--export([rating/2]).
+-export([rating/3]).
 
 -include("ocs.hrl").
 
--spec rating(RequestType, SubscriptionRef) -> Return
-	when
-		RequestType :: price_request | tariff_request,
-		SubscriptionRef :: #subscriber{},
-		Return :: {ok, #subscriber{}} | {error, Reason},
-		Reason :: term().
-rating(tariff_request, #subscriber{buckets = Buckets,
-		product = #product_instance{product = ProdID},} = SubscriptionRef) ->
-	F = fun(#bucket{bucket_type = octets, remain_amount = RA}) when
-					RA#remain_amount.amount > 0 -> 
-				true;
-			(_) -> 
-				false
-	end,
+%% support deprecated_time_unit()
+-define(MILLISECOND, milli_seconds).
+%-define(MILLISECOND, millisecond).
 
-	rating1(lists:any(F, Buckets), ProdID, Buckets, SubscriptionRef).
-%% @hidden
-rating1(false, ProdID, Buckets, SubscriptionRef) ->
-	rating2(ocs:find_product(ProdID), Buckets, SubscriptionRef);
-rating1(true, _ProdID, _Buckets, SubscriptionRef) ->
-	{ok, SubscriptionRef}.
-%% @hidden
-rating2({ok, #product{price = Prices}}, Buckets, SubscriptionRef) ->
-	case lists:keyfind(usage, #price.type, Prices) of
-		#price{} = Price ->
-			rating3(Price, Buckets, SubscriptionRef);
-		false ->
-			{error, rating_failed}
-	end;
-rating2({error, Reason}, _Buckets, _SubscriptionRef) ->
-	{error, Reason}.
-%% @hidden
-rating3(#price{type = usage, size = Size, units = octets, amount = Amount}, Buckets, SubscriptionRef) ->
-	rating4(Amount, Size, Buckets, SubscriptionRef);
-rating3(_Price, _Buckets, _SubscriptionRef) ->
-	{error, rating_failed}.
-%% @hidden
-rating4(Amount, Size, Buckets, SubscriptionRef) ->
-	case lists:keytake(cents, #bucket.bucket_type, Buckets) of
-		{value, #bucket{remain_amount = 
-				#remain_amount{amount = Cents}} = RecuBucket, ReBuckets} when Cents >= Amount ->
-			B1 = #bucket{bucket_type = octets, remain_amount = #remain_amount{amount = Size}},
-			B2 = RecuBucket#bucket{remain_amount = #remain_amount{amount = Cents - Amount}},
-			NewBuckets = [B1, B2 | ReBuckets],
-			{ok, SubscriptionRef#subscriber{buckets = NewBuckets}};
-		_ ->
-			{error, rating_failed}
+-spec rating(SubscriberID, UsageSecs, UsageOctets) -> Return
+	when
+		SubscriberID :: string() | binary(),
+		UsageSecs :: integer(),
+		UsageOctets :: integer(),
+		Return :: ok | {error, Reason},
+		Reason :: term().
+rating(SubscriberID, UsageSecs, UsageOctets) when is_list(SubscriberID) ->
+	rating(list_to_binary(SubscriberID), UsageSecs, UsageOctets);
+rating(SubscriberID, UsageSecs, UsageOctets) when is_binary(SubscriberID) ->
+	F = fun() ->
+			case mnesia:read(subscriber, SubscriberID, write) of
+				[#subscriber{buckets = Buckets, product =
+						#product_instance{product = ProdID}} = Subscriber] ->
+					case mnesia:read(product, ProdID, read) of
+						[#product{price = Prices}] ->
+							case lists:keyfind(usage, #price.type, Prices) of
+								#price{} = Price ->
+									{Charged, NewBuckets} = rating2(Price, UsageSecs, UsageOctets, Buckets),
+									Entry = Subscriber#subscriber{buckets = NewBuckets},
+									mnesia:write(Entry);
+								false ->
+									throw(price_not_found)
+							end;
+						[] ->
+							throw(product_not_found)
+					end;
+				[] ->
+					throw(subscriber_not_found)
+			end
+	end,
+	case mnesia:transaction(F) of
+		{atomic, _} ->
+			ok;
+		{aborted, {throw, Reason}} ->
+			{error, Reason};
+		{aborted, Reason} ->
+			{error, Reason}
 	end.
+%% @hidden
+rating2(#price{type = usage, size = Size, units = octets,
+		amount = Amount}, _UsageSecs, UsageOctets, Buckets) ->
+	rating3(Amount, Size, octets, UsageOctets, Buckets);
+rating2(#price{type = usage, size = Size, units = seconds,
+		amount = Amount}, UsageSecs, _UsageOctets, Buckets) ->
+	rating3(Amount, Size, seconds, UsageSecs, Buckets).
+%% @hidden
+rating3(Price, Size, Type, Used, Buckets) ->
+	Validity = erlang:system_time(?MILLISECOND) + 245200,
+	case charge(Type, Used, lists:sort(fun sort_buckets/2, Buckets)) of
+		{Charged, NewBuckets} when Charged < Used ->
+			purchase(Type, Price, Size, Used - Charged, Validity, NewBuckets);
+		{Charged, NewBuckets} ->
+			{Charged, NewBuckets}
+	end.
+
+%%----------------------------------------------------------------------
+%%  internal functions
+%%----------------------------------------------------------------------
+-spec charge(Type, Charge, Buckets) -> Result
+	when
+		Type :: octets | seconds | cents,
+		Charge :: integer(),
+		Buckets :: [#bucket{}],
+		Result :: {Charged, Buckets},
+		Charged :: integer().
+charge(Type, Charge, Buckets) ->
+	Now = erlang:system_time(?MILLISECOND),
+	charge(Type, Charge, Now, Buckets, [], 0).
+%% @hidden
+charge(Type, Charge, Now, [#bucket{bucket_type = Type,
+		termination_date = T1} | T], Acc, Charged) when T1 =/= undefined, T1 =< Now->
+	charge(Type, Charge, Now, T, Acc, Charged);
+charge(Type, Charge, _Now, [#bucket{bucket_type = Type,
+		remain_amount = R} = B | T], Acc, Charged) when R > Charge ->
+	NewBuckets = [B#bucket{remain_amount = R - Charge} | T],
+	{Charged + Charge, NewBuckets ++ Acc};
+charge(Type, Charge, Now, [#bucket{bucket_type = Type,
+		remain_amount = R} | T], Acc, Charged) when R =< Charge ->
+	charge(Type, Charge - R, Now, T, Acc, Charged + R);
+charge(_Type, 0, _Now, Buckets, Acc, Charged) ->
+	{Charged, Buckets ++ Acc};
+charge(_Type, _Charge, _Now, [], Acc, Charged) ->
+	{Charged, Acc};
+charge(_Type, _Charge, _Now, Buckets, Acc, Charged) ->
+	{Charged, Buckets ++ Acc}.
+
+-spec purchase(Type, Price, Size, Used, Validity, Buckets) -> Result
+	when
+		Type :: octets | seconds,
+		Price :: integer(),
+		Size :: integer(),
+		Used :: integer(),
+		Validity :: integer(),
+		Buckets :: [#bucket{}],
+		Result :: {Charged, Buckets},
+		Charged :: integer().
+purchase(Type, Price, Size, Used, Validity, Buckets) ->
+	UnitsNeeded = case (Used rem Size) of
+		0 ->
+			Used div Size;
+		_ ->
+			(Used div Size) + 1
+	end,
+	Charge = UnitsNeeded * Price,
+	case charge(cents, Charge, Buckets) of
+		{Charged, NewBuckets} when Charged < Charge ->
+			{Charged, NewBuckets};
+		{Charged, NewBuckets} ->
+			Remain = UnitsNeeded * Size - Used,
+			Bucket = #bucket{bucket_type = Type, remain_amount = Remain,
+				termination_date = Validity,
+				start_date = erlang:system_time(?MILLISECOND)},
+			{Charged, [Bucket | NewBuckets]}
+	end.
+
+%% @hidden
+sort_buckets(#bucket{termination_date = T1}, #bucket{termination_date = T2}) when
+		T1 =< T2->
+	true;
+sort_buckets(_, _)->
+	false.
+
