0fe59bdd9bb6ef7572d0542c9cd8b4cfca4f3c46
Prahveen Thiruchelvam <prahveen@sigscale.org>
2017-10-27 12:43:47 +0530

    add test case for rate with usage debiting

--- test/ocs_rating_SUITE.erl
+++ test/ocs_rating_SUITE.erl
@@ -29,6 +29,7 @@
 -compile(export_all).
 
 -include("ocs.hrl").
+-include_lib("radius/include/radius.hrl").
 -include_lib("common_test/include/ct.hrl").
 
 %% support deprecated_time_unit()
@@ -83,11 +84,200 @@ sequences() ->
 %% Returns a list of all test cases in this test suite.
 %%
 all() -> 
-	[].
+	[rate_octets_with_debiting_scenario_1, rate_octets_with_debiting_scenario_2,
+	rate_octets_with_debiting_scenario_3, rate_octets_with_debiting_scenario_4,
+	rate_octets_with_debiting_scenario_5, rate_octets_with_debiting_scenario_6,
+	rate_octets_with_debiting_scenario_7].
 
 %%---------------------------------------------------------------------
 %%  Test cases
 %%---------------------------------------------------------------------
+rate_octets_with_debiting_scenario_1() ->
+	[{userdata, [{doc, "Rate cents buckets to octets
+		and deduct usage from rated buckets, This senario
+		describ debit amount equal to package size"}]}].
+
+rate_octets_with_debiting_scenario_1(_Config) ->
+	ProdID = ocs:generate_password(),
+	PackagePrice = 100,
+	Price = #price{name = "overage", type = usage,
+		units = octets, size = 1000, amount = PackagePrice},
+	Product = #product{name = ProdID, price = [Price]},
+	{ok, _} = ocs:add_product(Product),
+	SubscriberID = list_to_binary(ocs:generate_identity()),
+	Password = ocs:generate_password(),
+	Chars = [{validity, erlang:system_time(?MILLISECOND) + 2592000000}],
+	RemAmount = 200,
+	Debit = 1000,
+	Buckets = [#bucket{bucket_type = cents, remain_amount = RemAmount,
+		start_date = erlang:system_time(?MILLISECOND),
+		termination_date = erlang:system_time(?MILLISECOND) + 2592000000}],
+	{ok, _} = ocs:add_subscriber(SubscriberID, Password, ProdID, Chars, Buckets),
+	{ok, _, _} = ocs_rating:rate(SubscriberID, final, [{octets, Debit}], []),
+	{ok, #subscriber{buckets = RatedBuckets}} = ocs:find_subscriber(SubscriberID),
+	#bucket{remain_amount = CentsRemain} = lists:keyfind(cents, #bucket.bucket_type, RatedBuckets),
+	CentsRemain = RemAmount - PackagePrice,
+	false = lists:keyfind(octets, #bucket.bucket_type, RatedBuckets).
+
+rate_octets_with_debiting_scenario_2() ->
+	[{userdata, [{doc, "Rate cents buckets to octets
+		and deduct usage from rated buckets, This senario
+		describ debit amount less than package size"}]}].
+
+rate_octets_with_debiting_scenario_2(_Config) ->
+	ProdID = ocs:generate_password(),
+	PackagePrice = 100,
+	PackageSize = 1000,
+	Price = #price{name = "overage", type = usage,
+		units = octets, size = PackageSize, amount = PackagePrice},
+	Product = #product{name = ProdID, price = [Price]},
+	{ok, _} = ocs:add_product(Product),
+	SubscriberID = list_to_binary(ocs:generate_identity()),
+	Password = ocs:generate_password(),
+	Chars = [{validity, erlang:system_time(?MILLISECOND) + 2592000000}],
+	RemAmount = 200,
+	Debit = 100,
+	Buckets = [#bucket{bucket_type = cents, remain_amount = RemAmount,
+		start_date = erlang:system_time(?MILLISECOND),
+		termination_date = erlang:system_time(?MILLISECOND) + 2592000000}],
+	{ok, _} = ocs:add_subscriber(SubscriberID, Password, ProdID, Chars, Buckets),
+	{ok, _, _} = ocs_rating:rate(SubscriberID, final, [{octets, Debit}], []),
+	{ok, #subscriber{buckets = RatedBuckets}} = ocs:find_subscriber(SubscriberID),
+	#bucket{remain_amount = CentsRemain} = lists:keyfind(cents, #bucket.bucket_type, RatedBuckets),
+	CentsRemain = RemAmount - PackagePrice,
+	#bucket{remain_amount = OctetsRemain} = lists:keyfind(octets, #bucket.bucket_type, RatedBuckets),
+	OctetsRemain = PackageSize - Debit.
+
+rate_octets_with_debiting_scenario_3() ->
+	[{userdata, [{doc, "Rate cents buckets to octets
+		and deduct usage from rated buckets, This senario
+		describ debit amount equal to bucket remain amount and
+		package size"}]}].
+
+rate_octets_with_debiting_scenario_3(_Config) ->
+	ProdID = ocs:generate_password(),
+	PackagePrice = 100,
+	PackageSize = 1000,
+	Price = #price{name = "overage", type = usage,
+		units = octets, size = PackageSize, amount = PackagePrice},
+	Product = #product{name = ProdID, price = [Price]},
+	{ok, _} = ocs:add_product(Product),
+	SubscriberID = list_to_binary(ocs:generate_identity()),
+	Password = ocs:generate_password(),
+	Chars = [{validity, erlang:system_time(?MILLISECOND) + 2592000000}],
+	RemAmount = 100,
+	Debit = 1000,
+	Buckets = [#bucket{bucket_type = cents, remain_amount = RemAmount,
+		start_date = erlang:system_time(?MILLISECOND),
+		termination_date = erlang:system_time(?MILLISECOND) + 2592000000}],
+	{ok, _} = ocs:add_subscriber(SubscriberID, Password, ProdID, Chars, Buckets),
+	{ok, _, _} = ocs_rating:rate(SubscriberID, final, [{octets, Debit}], []),
+	{ok, #subscriber{buckets = []}} = ocs:find_subscriber(SubscriberID).
+
+rate_octets_with_debiting_scenario_4() ->
+	[{userdata, [{doc, "Rate cents buckets to octets
+		and deduct usage from rated buckets, This senario
+		describ out of credit"}]}].
+
+rate_octets_with_debiting_scenario_4(_Config) ->
+	ProdID = ocs:generate_password(),
+	Price = #price{name = "overage", type = usage,
+		units = octets, size = 1000, amount = 100},
+	Product = #product{name = ProdID, price = [Price]},
+	{ok, _} = ocs:add_product(Product),
+	SubscriberID = list_to_binary(ocs:generate_identity()),
+	Password = ocs:generate_password(),
+	Chars = [{validity, erlang:system_time(?MILLISECOND) + 2592000000}],
+	RemAmount = 10,
+	Debit = 100,
+	Buckets = [#bucket{bucket_type = cents, remain_amount = RemAmount,
+		start_date = erlang:system_time(?MILLISECOND),
+		termination_date = erlang:system_time(?MILLISECOND) + 2592000000}],
+	{ok, _} = ocs:add_subscriber(SubscriberID, Password, ProdID, Chars, Buckets),
+	{out_of_credit, _} = ocs_rating:rate(SubscriberID, final, [{octets, Debit}], []),
+	{ok, #subscriber{buckets = []}} = ocs:find_subscriber(SubscriberID).
+
+rate_octets_with_debiting_scenario_5() ->
+	[{userdata, [{doc, "This senario describ when out of
+		credit remove session attributes from subscriber record"}]}].
+
+rate_octets_with_debiting_scenario_5(_Config) ->
+	ProdID = ocs:generate_password(),
+	Price = #price{name = "overage", type = usage,
+		units = octets, size = 1000, amount = 100},
+	Product = #product{name = ProdID, price = [Price]},
+	{ok, _} = ocs:add_product(Product),
+	SubscriberID = list_to_binary(ocs:generate_identity()),
+	Password = ocs:generate_password(),
+	Chars = [{validity, erlang:system_time(?MILLISECOND) + 2592000000}],
+	RemAmount = 10,
+	Debit = 100,
+	Buckets = [#bucket{bucket_type = cents, remain_amount = RemAmount,
+		start_date = erlang:system_time(?MILLISECOND),
+		termination_date = erlang:system_time(?MILLISECOND) + 2592000000}],
+	SessionAttributes = [{erlang:system_time(?MILLISECOND), [{?AcctSessionId, "1020303"},
+		{?NasIdentifier, "rate@sigscale"}, {?NasIpAddress, "10.0.0.1"}]}],
+	Subscriber = #subscriber{name = SubscriberID, password = Password,
+			buckets = Buckets, session_attributes = SessionAttributes,
+			product = #product_instance{product = ProdID, characteristics = Chars}},
+	mnesia:dirty_write(subscriber, Subscriber),
+	{out_of_credit, SessionAttributes} = ocs_rating:rate(SubscriberID, final, [{octets, Debit}], []),
+	{ok, #subscriber{session_attributes = []}} = ocs:find_subscriber(SubscriberID).
+
+rate_octets_with_debiting_scenario_6() ->
+	[{userdata, [{doc, "This senario describ when final call
+		remove given session attributes from subscriber record"}]}].
+
+rate_octets_with_debiting_scenario_6(_Config) ->
+	ProdID = ocs:generate_password(),
+	Price = #price{name = "overage", type = usage,
+		units = octets, size = 1000, amount = 100},
+	Product = #product{name = ProdID, price = [Price]},
+	{ok, _} = ocs:add_product(Product),
+	SubscriberID = list_to_binary(ocs:generate_identity()),
+	Password = ocs:generate_password(),
+	Chars = [{validity, erlang:system_time(?MILLISECOND) + 2592000000}],
+	RemAmount = 1000,
+	Debit = 100,
+	Buckets = [#bucket{bucket_type = cents, remain_amount = RemAmount,
+		start_date = erlang:system_time(?MILLISECOND),
+		termination_date = erlang:system_time(?MILLISECOND) + 2592000000}],
+	SA1 = {erlang:system_time(?MILLISECOND), [{?AcctSessionId, "1020303"},
+		{?NasIdentifier, "rate1@sigscale"}, {?NasIpAddress, "10.0.0.1"}]},
+	SA2 = {erlang:system_time(?MILLISECOND), [{?AcctSessionId, "1020304"},
+		{?NasIdentifier, "rate2@sigscale"}, {?NasIpAddress, "10.0.0.2"}]},
+	Identification = [{?AcctSessionId, "1020303"},
+		{?NasIdentifier, "rate1@sigscale"}, {?NasIpAddress, "10.0.0.1"}],
+	SessionAttributes = [SA1, SA2],
+	Subscriber = #subscriber{name = SubscriberID, password = Password,
+			buckets = Buckets, session_attributes = SessionAttributes,
+			product = #product_instance{product = ProdID, characteristics = Chars}},
+	mnesia:dirty_write(subscriber, Subscriber),
+	{ok, _, _} = ocs_rating:rate(SubscriberID, final, [{octets, Debit}], [], Identification),
+	{ok, #subscriber{session_attributes = [SA2]}} = ocs:find_subscriber(SubscriberID).
+
+rate_octets_with_debiting_scenario_7() ->
+	[{userdata, [{doc, "This senario describ when inital call for the
+		rate function include the session attributes"}]}].
+
+rate_octets_with_debiting_scenario_7(_Config) ->
+	ProdID = ocs:generate_password(),
+	Price = #price{name = "overage", type = usage,
+		units = octets, size = 1000, amount = 100},
+	Product = #product{name = ProdID, price = [Price]},
+	{ok, _} = ocs:add_product(Product),
+	SubscriberID = list_to_binary(ocs:generate_identity()),
+	Password = ocs:generate_password(),
+	Chars = [{validity, erlang:system_time(?MILLISECOND) + 2592000000}],
+	Buckets = [#bucket{bucket_type = cents, remain_amount = 200,
+		start_date = erlang:system_time(?MILLISECOND),
+		termination_date = erlang:system_time(?MILLISECOND) + 2592000000}],
+	{ok, _} = ocs:add_subscriber(SubscriberID, Password, ProdID, Chars, Buckets),
+	SessionAttributes = [{?AcctSessionId, "1020303"}, {?NasIdentifier, "rate@sigscale"},
+		{?NasIpAddress, "10.0.0.1"}],
+	{ok, _, _} = ocs_rating:rate(SubscriberID, inital, [{octets, 100}], [], SessionAttributes),
+	{ok, #subscriber{session_attributes = SessionList}} = ocs:find_subscriber(SubscriberID),
+	{_, SessionAttributes} = lists:nth(1, SessionList).
 
 %%---------------------------------------------------------------------
 %%  Internal functions
