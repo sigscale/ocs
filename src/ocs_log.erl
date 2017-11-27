f77a1a4d1d27e1ad43062618a040535fa50589fe
Vance Shipley <vances@sigscale.org>
2017-11-27 11:41:49 +0530

    convert is08601 to epoch milliseconds

--- src/ocs_log.erl
+++ src/ocs_log.erl
@@ -1052,18 +1052,122 @@ date(MilliSeconds) when is_integer(MilliSeconds) ->
 	Seconds = ?EPOCH + (MilliSeconds div 1000),
 	calendar:gregorian_seconds_to_datetime(Seconds).
 
--spec iso8601(MilliSeconds) -> Result
+-spec iso8601(DateTime) -> DateTime
 	when
-		MilliSeconds :: pos_integer(),
-		Result :: string().
-%% @doc Convert timestamp to ISO 8601 format date and time.
-iso8601(MilliSeconds) when is_integer(MilliSeconds) ->
-	{{Year, Month, Day}, {Hour, Minute, Second}} = date(MilliSeconds),
+		DateTime :: pos_integer() | string().
+%% @doc Convert between ISO 8601 and Unix epoch milliseconds.
+%% 	Parsing is not strict to allow prefix matching.
+iso8601(DateTime) when is_integer(DateTime) ->
+	{{Year, Month, Day}, {Hour, Minute, Second}} = date(DateTime),
 	DateFormat = "~4.10.0b-~2.10.0b-~2.10.0b",
 	TimeFormat = "T~2.10.0b:~2.10.0b:~2.10.0b.~3.10.0bZ",
 	Chars = io_lib:fwrite(DateFormat ++ TimeFormat,
-			[Year, Month, Day, Hour, Minute, Second, MilliSeconds rem 1000]),
-	lists:flatten(Chars).
+			[Year, Month, Day, Hour, Minute, Second, DateTime rem 1000]),
+	lists:flatten(Chars);
+iso8601([Y1, Y2, Y3, Y4 | T])
+		when Y1 >= $0, Y1 =< $9, Y2 >= $0, Y2 =< $9,
+		Y3 >= $0, Y3 =< $9, Y4 >= $0, Y4 =< $9 ->
+	iso8601month(list_to_integer([Y1, Y2, Y3, Y4]), T).
+%% @hidden
+iso8601month(Year, []) ->
+	DateTime = {{Year, 1, 1}, {0, 0, 0}},
+	GS = calendar:datetime_to_gregorian_seconds(DateTime),
+	(GS - ?EPOCH) * 1000;
+iso8601month(Year, [$-]) ->
+	iso8601month(Year, []);
+iso8601month(Year, [$-, $0]) ->
+	iso8601month(Year, [$-, $0, $1]);
+iso8601month(Year, [$-, $1]) ->
+	iso8601month(Year, [$-, $1, $0]);
+iso8601month(Year, [$-, M1, M2 | T])
+		when M1 >= $0, M1 =< $1, M2 >= $0, M2 =< $9 ->
+	iso8601day(Year, list_to_integer([M1, M2]), T).
+%% @hidden
+iso8601day(Year, Month, []) ->
+	DateTime = {{Year, Month, 1}, {0, 0, 0}},
+	GS = calendar:datetime_to_gregorian_seconds(DateTime),
+	(GS - ?EPOCH) * 1000;
+iso8601day(Year, Month, [$-]) ->
+	iso8601day(Year, Month, []);
+iso8601day(Year, Month, [$-, $0]) ->
+	iso8601day(Year, Month, [$-, $1, $0]);
+iso8601day(Year, Month, [$-, D1])
+		when D1 >= $1, D1 =< $3 ->
+	iso8601day(Year, Month, [$-, D1, $0]);
+iso8601day(Year, Month, [$-, D1, D2 | T])
+		when D1 >= $0, D1 =< $3, D2 >= $0, D2 =< $9 ->
+	Day = list_to_integer([D1, D2]),
+	iso8601hour({Year, Month, Day}, T).
+%% @hidden
+iso8601hour(Date, []) ->
+	DateTime = {Date, {0, 0, 0}},
+	GS = calendar:datetime_to_gregorian_seconds(DateTime),
+	(GS - ?EPOCH) * 1000;
+iso8601hour(Date, [$T]) ->
+	iso8601hour(Date, []);
+iso8601hour(Date, [$T, H1])
+		when H1 >= $0, H1 =< $2 ->
+	iso8601hour(Date, [$T, H1, $0]);
+iso8601hour(Date, [$T, H1, H2 | T])
+		when H1 >= $0, H1 =< $2, H2 >= $0, H2 =< $9 ->
+	Hour = list_to_integer([H1, H2]),
+	iso8601minute(Date, Hour, T).
+%% @hidden
+iso8601minute(Date, Hour, []) ->
+	DateTime = {Date, {Hour, 0, 0}},
+	GS = calendar:datetime_to_gregorian_seconds(DateTime),
+	(GS - ?EPOCH) * 1000;
+iso8601minute(Date, Hour, [$:]) ->
+	iso8601minute(Date, Hour, []);
+iso8601minute(Date, Hour, [$:, M1])
+		when M1 >= $0, M1 =< $5 ->
+	iso8601minute(Date, Hour, [$:, M1, $0]);
+iso8601minute(Date, Hour, [$:, M1, M2 | T])
+		when M1 >= $0, M1 =< $5, M2 >= $0, M2 =< $9 ->
+	Minute = list_to_integer([M1, M2]),
+	iso8601second(Date, Hour, Minute, T);
+iso8601minute(Date, Hour, _) ->
+	DateTime = {Date, {Hour, 0, 0}},
+	GS = calendar:datetime_to_gregorian_seconds(DateTime),
+	(GS - ?EPOCH) * 1000.
+%% @hidden
+iso8601second(Date, Hour, Minute, []) ->
+	DateTime = {Date, {Hour, Minute, 0}},
+	GS = calendar:datetime_to_gregorian_seconds(DateTime),
+	(GS - ?EPOCH) * 1000;
+iso8601second(Date, Hour, Minute, [$:]) ->
+	iso8601second(Date, Hour, Minute, []);
+iso8601second(Date, Hour, Minute, [$:, S1])
+		when S1 >= $0, S1 =< $5 ->
+	iso8601second(Date, Hour, Minute, [$:, S1, $0]);
+iso8601second(Date, Hour, Minute, [$:, S1, S2 | T])
+		when S1 >= $0, S1 =< $5, S2 >= $0, S2 =< $9 ->
+	Second = list_to_integer([S1, S2]),
+	DateTime = {Date, {Hour, Minute, Second}},
+	GS = calendar:datetime_to_gregorian_seconds(DateTime),
+	EpocMilliseconds = (GS - ?EPOCH) * 1000,
+	iso8601millisecond(EpocMilliseconds, T);
+iso8601second(Date, Hour, Minute, _) ->
+	DateTime = {Date, {Hour, Minute, 0}},
+	GS = calendar:datetime_to_gregorian_seconds(DateTime),
+	(GS - ?EPOCH) * 1000.
+%% @hidden
+iso8601millisecond(EpocMilliseconds, []) ->
+	EpocMilliseconds;
+iso8601millisecond(EpocMilliseconds, [$.]) ->
+	EpocMilliseconds;
+iso8601millisecond(EpocMilliseconds, [$., N1, N2, N3 | _])
+		when N1 >= $0, N1 =< $9, N2 >= $0, N2 =< $9,
+		N3 >= $0, N3 =< $9 ->
+	EpocMilliseconds + list_to_integer([N1, N2, N3]);
+iso8601millisecond(EpocMilliseconds, [$., N1, N2 | _])
+		when N1 >= $0, N1 =< $9, N2 >= $0, N2 =< $9 ->
+	EpocMilliseconds + list_to_integer([N1, N2]) * 10;
+iso8601millisecond(EpocMilliseconds, [$., N | _])
+		when N >= $0, N =< $9 ->
+	EpocMilliseconds + list_to_integer([N]) * 100;
+iso8601millisecond(EpocMilliseconds, _) ->
+	EpocMilliseconds.
 
 uuid() ->
 	<<A:32, B:16, C:16, D:16, E:48>> = crypto:strong_rand_bytes(16),
