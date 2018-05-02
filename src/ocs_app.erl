2466987e8f7898092e9a86ad891457c5857b2992
Vance Shipley <vances@sigscale.org>
2018-05-02 20:25:03 +0530

    don't wait for mod_auth tables

--- src/ocs_app.erl
+++ src/ocs_app.erl
@@ -55,8 +55,7 @@
 %% @see //kernel/application:start/2
 %%
 start(normal = _StartType, _Args) ->
-	Tables = [client, service, offer, product,
-			pla, httpd_user, httpd_group],
+	Tables = [client, service, offer, product, pla],
 	case mnesia:wait_for_tables(Tables, 60000) of
 		ok ->
 			start1();
