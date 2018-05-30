a5feab768a62229bbe157de741f660fe53626cc9
Vance Shipley <vances@sigscale.org>
2018-05-30 19:54:39 +0530

    handle codec error

--- src/ocs_rest_res_balance.erl
+++ src/ocs_rest_res_balance.erl
@@ -177,7 +177,7 @@ top_up_service(Identity, RequestBody) ->
 	try
 		bucket(mochijson:decode(RequestBody))
 	of
-		Bucket ->
+		#bucket{} = Bucket ->
 			case ocs:find_service(Identity) of
 				{ok, #service{product = ProductRef}} ->
 					case ocs:add_bucket(ProductRef, Bucket) of
