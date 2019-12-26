59654d1b53c63aec5bf631a7e046d2a691596a97
Vance Shipley <vances@sigscale.org>
2019-12-27 05:26:32 +0800

    constant h array values

--- c_src/ocs_eap_aka.c
+++ c_src/ocs_eap_aka.c
@@ -41,15 +41,17 @@ g_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
    ErlNifBinary xkey, res;
    ERL_NIF_TERM reason;
    int i;
-   static uint32_t h[5] = {0x67452301,
+   const uint32_t h[5] = {0x67452301,
 			0xefcdab89, 0x98badcfe, 0x10325476, 0xc3d2e1f0};
-   uint32_t a, b, c, d, e, temp, m[16], w[80];
+   uint32_t a, b, c, d, e, temp;
+   uint32_t m[16] = {0};
+   uint32_t w[80] = {0};
    
    if (!enif_inspect_binary(env, argv[0], &xkey) || xkey.size != 20)
       return enif_make_badarg(env);
    for(i = 0; i < 5; i++) {
-      m[i] = (xkey.data[i*4] << 24) + (xkey.data[i*4+1] << 16)
-            + (xkey.data[i*4+2] << 8) + (xkey.data[i*4+3]);
+      m[i] = (xkey.data[i*4] << 24) | (xkey.data[i*4+1] << 16)
+            | (xkey.data[i*4+2] << 8) | (xkey.data[i*4+3]);
    } 
    m[5] = 0x80000000; 
    for(i = 6; i < 15; i++) {
@@ -74,6 +76,7 @@ g_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 		c = CLS(30, b);
 		b = a;
 		a = temp;
+
 	}
 	for(i = 20; i < 40; i++) {
 		temp = CLS(5, a) + F2(a, b, c) + e + w[i] + 0x6ed9eba1;
@@ -99,21 +102,35 @@ g_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 		b = a;
 		a = temp;
 	}
-	h[0] = h[0] + a;
-	h[1] = h[1] + b;
-	h[2] = h[2] + c;
-	h[3] = h[3] + d;
-	h[4] = h[4] + e;
 	if (!enif_alloc_binary(20, &res)) {
 		enif_make_existing_atom(env, "enomem", &reason, ERL_NIF_LATIN1);
 		return enif_raise_exception(env, reason);
 	}
-	for(i = 0; i < 5; i++) {
-		res.data[i*4] = h[i] >> 24;
-		res.data[i*4+1] = (h[i] >> 16) & 0xff;
-		res.data[i*4+2] = (h[i] >> 8) & 0xff;
-		res.data[i*4+3] = h[i] & 0xff;
-	}
+	temp = h[0] + a;
+	res.data[0] = temp >> 24;
+	res.data[1] = (temp >> 16) & 0xff;
+	res.data[2] = (temp >> 8) & 0xff;
+	res.data[3] = temp & 0xff;
+	temp = h[1] + b;
+	res.data[4] = temp >> 24;
+	res.data[5] = (temp >> 16) & 0xff;
+	res.data[6] = (temp >> 8) & 0xff;
+	res.data[7] = temp & 0xff;
+	temp = h[2] + c;
+	res.data[8] = temp >> 24;
+	res.data[9] = (temp >> 16) & 0xff;
+	res.data[10] = (temp >> 8) & 0xff;
+	res.data[11] = temp & 0xff;
+	temp = h[3] + d;
+	res.data[12] = temp >> 24;
+	res.data[13] = (temp >> 16) & 0xff;
+	res.data[14] = (temp >> 8) & 0xff;
+	res.data[15] = temp & 0xff;
+	temp = h[4] + e;
+	res.data[16] = temp >> 24;
+	res.data[17] = (temp >> 16) & 0xff;
+	res.data[18] = (temp >> 8) & 0xff;
+	res.data[19] = temp & 0xff;
 	return enif_make_binary(env, &res);
 }
 
