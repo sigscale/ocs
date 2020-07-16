486031b99f46f228edaa6a9582dfaf8d31212120
Krishmanl <krishmanl@sigscale.com>
2020-07-16 11:44:18 +0000

    upgrade service list to polymer3

--- priv/www/src/sig-sub-list.js
+++ priv/www/src/sig-sub-list.js
@@ -1,365 +1,257 @@
-<!--  vim: set ts=3:  -->
-<link rel="import" href="polymer/polymer.html">
-<link rel="import" href="vaadin-grid/vaadin-grid.html">
-<link rel="import" href="vaadin-grid/vaadin-grid-filter.html">
-<link rel="import" href="vaadin-grid/vaadin-grid-column-group.html">
-<link rel="import" href="i18n-msg/i18n-msg-behavior.html">
-<link rel="import" href="iron-ajax/iron-ajax.html">
-<link rel="import" href="paper-fab/paper-fab.html" >
-<link rel="import" href="paper-toast/paper-toast.html">
-<link rel="import" href="paper-styles/color.html">
-<link rel="import" href="sig-sub-add.html">
+/**
+ * @license
+ * Copyright (c) 2020 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
 
-<dom-module id="sig-sub-list">
-	<template>
-		<style is="custom-style">
-			::-webkit-input-placeholder { /* Chrome/Opera/Safari */
-				color: initial;
-				font-weight: bold;
-			}
-			::-moz-placeholder { /* Firefox 19+ */
-				color: initial;
-				font-weight: bold;
-			}
-			:-ms-input-placeholder { /* IE 10+ */
-				color: initial;
-				font-weight: bold;
-			}
-			:-moz-placeholder { /* Firefox 18- */
-				color: initial;
-				font-weight: bold;
-			}
-			.add-button {
-				right: 2%;
-				position: fixed;
-				bottom: 5%;
-				z-index: 100;
-			}
-			paper-fab {
-				background: var(--paper-lime-a700);
-				color: black;
-			}
-			vaadin-grid {
-				height: 100%;
-				--vaadin-grid-header-cell: {
-					background: #ffb04c;
-				};
-			}
-			vaadin-grid .grouptitle {
-				text-align: center;
-				border-bottom-style: solid;
-				border-color: var(--paper-yellow-900);
-			}
-			vaadin-grid .cell.numeric{
-				text-align: right;
-			}
-			vaadin-grid input {
-				font-size: inherit;
-				background: #ffb04c;
-				border-style: none;
-			}
-			.yellow-button {
-				text-transform: none;
-				color: #eeff41;
-			}
-		</style>
-		<vaadin-grid id="subscriberGrid" active-item="{{activeItem}}">
-			<vaadin-grid-column width="15ex" flex-grow="5">
-				<template class="header">
-					<vaadin-grid-filter aria-label="[[i18n.identity]]" path="id" value="[[_filterIdentity]]">
-						<input placeholder="[[i18n.identity]]" value="{{_filterIdentity::input}}" focus-target>
-					</vaadin-grid-filter>
-				</template>
-				<template>[[item.id]]</template>
-			</vaadin-grid-column>
-			<vaadin-grid-column width="14ex">
-				<template class="header">
-					<i18n-msg msgid="password">
-						Password
-					</i18n-msg>
-				</template>
-				<template>[[item.password]]</template>
-			</vaadin-grid-column>
-			<vaadin-grid-column width="8ex" flex-grow="5">
-				<template class="header">
-					<vaadin-grid-filter aria-label="[[i18n.prod]]" path="product" value="[[_filterBalance]]">
-						<input placeholder="[[i18n.prod]]" value="{{_filterBalance::input}}" focus-target>
-					</vaadin-grid-filter>
-				</template>
-			<template>[[item.product]]</template>
-			</vaadin-grid-column>
-			<vaadin-grid-column width="8ex" flex-grow="1">
-				<template class="header">
-					<i18n-msg msgid="enable">
-						Enabled
-					</i18n-msg>
-				</template>
-				<template>[[item.enabled]]</template>
-			</vaadin-grid-column>
-			<vaadin-grid-column width="8ex" flex-grow="1">
-				<template class="header">
-					<i18n-msg msgid="multi">
-						Multisession
-					</i18n-msg>
-				</template>
-				<template>[[item.multisession]]</template>
-			</vaadin-grid-column>
-		</vaadin-grid>
-		<div class="add-button">
-			<paper-fab
-					icon="add"
-					on-tap="showAddModal">
-			</paper-fab>
-		</div>
-		<paper-toast
-				id="addSubscriberToastSuccess"
-				text="[[i18n.subscriberAdded]]">
-		</paper-toast>
-		<paper-toast
-				id="updateSubscriberToastSuccess"
-				text="[[i18n.subscriberUpdated]]">
-		</paper-toast>
-		<paper-toast
-				id="deleteSubscriberToastSuccess"
-				text="[[i18n.subscriberDeleted]]">
-		</paper-toast>
-		<paper-toast
-				id="addSubscriberProductToastError"
-				text="[[i18n.toastErrorSubPro]]">
-		</paper-toast>
-		<paper-toast id="getSubscriberErrorToast" duration="0">
-			<paper-button
-					class="yellow-button"
-					onclick="getSubscriberErrorToast.toggle()">
-				Close
-			</paper-button>
-		</paper-toast>
-		<iron-ajax
-			id="getSubscriberAjax"
-			url="/serviceInventoryManagement/v2/service"
-			rejectWithRequest>
-		</iron-ajax>
-		<iron-ajax
-			id="getProductInventoryAjax"
-			on-response="_getProductResponse"
-			on-error="_getProductError">
-		</iron-ajax>
-	</template>
-	<script>
-		var cbSubscriber;
-		var etag;
-		var lastItem;
-		Polymer ({
-			is: 'sig-sub-list',
-			behaviors: [i18nMsgBehavior],
-			properties: {
-				activePage: {
-					type: Boolean,
-					value: false,
-					observer: '_activePageChanged'
-				},
-				activeItem: {
-					observer:'_activeItemChanged'
-				},
-				_filterIdentity: {
-					observer: '_filterChangedId'
-				},
-				_filterBalance: {
-					observer: '_filterChangedProduct'
-				}
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import '@polymer/iron-ajax/iron-ajax.js';
+import '@polymer/paper-fab/paper-fab.js';
+import '@polymer/iron-icons/iron-icons.js';
+import '@vaadin/vaadin-grid/vaadin-grid.js';
+import '@vaadin/vaadin-grid/vaadin-grid-filter.js';
+import '@vaadin/vaadin-grid/vaadin-grid-sorter.js';
+import '@vaadin/vaadin-grid/vaadin-grid-column-group.js';
+import './style-element.js'
+
+class subList extends PolymerElement {
+	static get template() {
+		return html`
+			<style include="style-element"></style>
+			<vaadin-grid
+					id="subscriberGrid"
+					loading="{{loading}}"
+					active-item="{{activeItem}}">
+				<vaadin-grid-column>
+					<template class="header">
+						<vaadin-grid-filter
+							id="filter"
+							aria-label="Identity"
+							path="id"
+							value="{{_filterIdentity}}">
+							<input
+									slot="filter"
+									placeholder="Identity"
+									value="{{_filterIdentity::input}}"
+									focus-target>
+						</vaadin-grid-filter>
+					</template>
+					<template>[[item.id]]</template>
+				</vaadin-grid-column>
+				<vaadin-grid-column>
+					<template class="header">
+							Password
+					</template>
+					<template>[[item.password]]</template>
+				</vaadin-grid-column>
+				<vaadin-grid-column>
+					<template class="header">
+						<vaadin-grid-filter
+								aria-label="Product"
+								path="product"
+								value="[[_filterBalance]]">
+							<input
+									slot="filter"
+									placeholder="Product"
+									value="{{_filterBalance::input}}"
+									focus-target>
+						</vaadin-grid-filter>
+					</template>
+					<template>[[item.product]]</template>
+				</vaadin-grid-column>
+				<vaadin-grid-column>
+					<template class="header">
+							Enabled
+					</template>
+					<template>[[item.enabled]]</template>
+				</vaadin-grid-column>
+				<vaadin-grid-column>
+					<template class="header">
+							Multisession
+					</template>
+					<template>[[item.multisession]]</template>
+				</vaadin-grid-column>
+			</vaadin-grid>
+			<div class="add-button">
+				<paper-fab
+						icon="add"
+						on-tap="showAddModal">
+				</paper-fab>
+			</div>
+			<iron-ajax
+					id="getSubscriberAjax"
+					url="/serviceInventoryManagement/v2/service"
+					rejectWithRequest>
+			</iron-ajax>
+			<iron-ajax
+					id="getProductInventoryAjax"
+					on-response="_getProductResponse"
+					on-error="_getProductError">
+			</iron-ajax>
+		`;
+	}
+
+	static get properties() {
+		return {
+			loading: {
+				type: Boolean,
+				notify: true
 			},
-			_activePageChanged: function(active) {
-				if (active) {
-					var grid = this.$.subscriberGrid;
-					grid.columns = [
-						{
-							name: "id"
-						},
-						{
-							name: "password"
-						},
-						{
-							name: "product"
-						},
-						{
-							name: "totalBalance"
-						},
-						{
-							name: "cents"
-						},
-						{
-							name: "seconds"
-						},
-						{
-							name: "ascendDataRate"
-						},
-						{
-							name: "ascendXmitRate"
-						},
-						{
-							name: "sessionTimeout"
-						},
-						{
-							name: "acctInterimInterval"
-						},
-						{
-							name: "class"
-						},
-						{
-							name: "enabled"
-						},
-						{
-							name: "multisession"
-						}
-					];
-					grid.dataProvider = this._getSubscriberResponse;
-				}
+			etag: {
+				type: String,
+				value: null
 			},
-			_filterChangedId: function(filter) {
-				this.etag = null;
-				delete this.$.getSubscriberAjax.headers['If-Range'];
-				this.$.subscriberGrid.size = 0;
+			activeItem: {
+				type: Object,
+				notify: true,
+				observer: '_activeItemChanged'
 			},
-			_filterChangedProduct: function(filter) {
-				this.etag = null;
-				delete this.$.getSubscriberAjax.headers['If-Range'];
-				this.$.subscriberGrid.size = 0;
+			_filterIdentity: {
+				type: Boolean,
+				observer: '_filterChanged'
 			},
-			_activeItemChanged: function(item) {
-				if (item != null){
-					var ajaxCh = document.getElementById("getServiceRespAjax");
-					ajaxCh.url = "/serviceInventoryManagement/v2/service/" + item.id;
-					ajaxCh.generateRequest();
-					document.getElementById("updateSubscriberProductsAjax").generateRequest();
-					this.$.subscriberGrid.selectedItems = item ? [item] : [];
-					document.getElementById("updateSubscriberModal").open();
-					document.getElementById("updateSubscriberId").value = item.id;
-					document.getElementById("updateSubscriberPassword").value = item.password;
-					document.getElementById("updatePro").value = item.product;
-					document.getElementById("updateSubscriberTimeout").value = item.sessionTimeout;
-					document.getElementById("updateSubscriberInterval").value = item.acctInterimInterval;
-					document.getElementById("updateSubscriberClass").value =  item.class;
-					document.getElementById("updateSubscriberEnabled").checked =  item.enabled;
-					document.getElementById("updateSubscriberMulti").checked =  item.multisession;
-				}
-			},
-			refreshSub: function() {
-				this.etag = null;
-				delete this.$.getSubscriberAjax.headers['If-Range'];
-				delete this.$.getSubscriberAjax.params['filter'];
-				this._filterIdentity = null;
-				this._filterBalance = null;
-			},
-			_getSubscriberResponse: function(params, callback) {
-				var grid = document.getElementById('subscriberGrid');
-				var ajax = document.getElementById('getSubscriberAjax'); 
-				delete ajax.params['filter'];
-				function checkHead(param) {
-					return param.path == "id" || param.path == "product";
-				}
-				params.filters.filter(checkHead).forEach(function(filter) {
-					if(filter.value) {
-						if(ajax.params['filter']) {
-							ajax.params['filter'] += "]," + filter.path + ".like=[" + filter.value + "%";
-						} else {
-							ajax.params['filter'] = "\"[{" + filter.path + ".like=[" + filter.value + "%";
-						}
-					}
-				});
-				if (ajax.params['filter']) {
-					ajax.params['filter'] += "]}]\"";
-				}
-				var serviceList = document.getElementById('serviceList');
-				var handleAjaxResponse = function(request) {
-					if (request) {
-						serviceList.etag = request.xhr.getResponseHeader('ETag');
-						var range = request.xhr.getResponseHeader('Content-Range');
-						var range1 = range.split("/");
-						var range2 = range1[0].split("-");
-						if (range1[1] != "*") {
-							grid.size = Number(range1[1]);
-						} else {
-							grid.size = Number(range2[1]) + grid.pageSize * 2;
-						}
-						var vaadinItems = new Array();
-						function checkChar(characteristic) {
-							return characteristic.name == "username";
-						}
-						for (var index in request.response) {
-							var newRecord = new Object();
-							newRecord.id = request.response[index].id;
-							if(request.response[index].product) {
-								newRecord.product = request.response[index].product;
-							} else {
-								newRecord.product = document.getElementById("subscriberAdd").productId;
-							}
-							if(request.response[index].attributes) {
-								request.response[index].attributes.forEach(
-									function(attrObj) {
-										newRecord[attrObj.name] = attrObj.value;
-									}
-								);
-							}
-							function checkPassword(pass) {
-								return pass.name == "servicePassword";
-							}
-							var indexPass = request.response[index].serviceCharacteristic.findIndex(checkPassword);
-							if(indexPass != -1) {
-								newRecord.password = request.response[index].serviceCharacteristic[indexPass].value;
-							}
-							newRecord.enabled = request.response[index].isServiceEnabled;
-							function checkMultisession(multi) {
-								return multi.name == "multiSession";
-							}
-							var indexMulti = request.response[index].serviceCharacteristic.findIndex(checkMultisession);
-							if(indexMulti != -1) {
-								newRecord.multisession = request.response[index].serviceCharacteristic[indexMulti].value;
-							}
-							vaadinItems[index] = newRecord;
-						}
-						callback(vaadinItems);
-					} else {
-						grid.size = 0;
-						callback([]);
-					}
-				};
-				var handleAjaxError = function(error) {
-					serviceList.etag = null;
-					var toast = document.getElementById('usageToastError');
-					toast.text = error;
-					toast.open();
-					if(!grid.size) {
-						grid.size = 0;
-					}
-					callback([]);
-				}
-				if (ajax.loading) {
-					ajax.lastRequest.completes.then(function(request) {
-						var startRange = params.page * params.pageSize + 1;
-						var endRange = startRange + params.pageSize - 1;
-						ajax.headers['Range'] = "items=" + startRange + "-" + endRange;
-						if (serviceList.etag && params.page > 0) {
-							ajax.headers['If-Range'] = serviceList.etag;
-						} else {
-							delete ajax.headers['If-Range'];
-						}
-						return ajax.generateRequest().completes;
-					}, handleAjaxError).then(handleAjaxResponse, handleAjaxError);
+			_filterBalance: {
+				type: Boolean,
+				observer: '_filterChanged'
+			}
+		}
+	}
+
+	ready() {
+		super.ready();
+		var grid = this.shadowRoot.getElementById('subscriberGrid');
+		grid.dataProvider = this._getSub;
+	}
+
+	_activeItemChanged(item) {
+		if(item) {
+			this.$.subscriberGrid.selectedItems = item ? [item] : [];
+		} else {
+			this.$.subscriberGrid.selectedItems = [];
+		}
+	}
+
+	_getSub(params, callback) {
+		var grid = this; 
+		var serviceList = document.body.querySelector('sig-app').shadowRoot.querySelector('sig-sub-list');
+		var ajax = serviceList.shadowRoot.getElementById('getSubscriberAjax'); 
+		delete ajax.params['filter'];
+		function checkHead(param) {
+			return param.path == "id" || param.path == "product";
+		}
+		params.filters.filter(checkHead).forEach(function(filter) {
+			if(filter.value) {
+				if(ajax.params['filter']) {
+					ajax.params['filter'] += "]," + filter.path + ".like=[" + filter.value + "%";
 				} else {
-					var startRange = params.page * params.pageSize + 1;
-					var endRange = startRange + params.pageSize - 1;
-					ajax.headers['Range'] = "items=" + startRange + "-" + endRange;
-					if (serviceList.etag && params.page > 0) {
-						ajax.headers['If-Range'] = serviceList.etag;
-					} else {
-						delete ajax.headers['If-Range'];
-					}
-					ajax.generateRequest().completes.then(handleAjaxResponse, handleAjaxError);
+					ajax.params['filter'] = "\"[{" + filter.path + ".like=[" + filter.value + "%";
 				}
-			},
-			showAddModal: function(event) {
-				document.getElementById("addServiceModal").open();
 			}
 		});
-	</script>
-</dom-module>
+		if (ajax.params['filter']) {
+			ajax.params['filter'] += "]}]\"";
+		}
+		var handleAjaxResponse = function(request) {
+			if (request) {
+				serviceList.etag = request.xhr.getResponseHeader('ETag');
+				var range = request.xhr.getResponseHeader('Content-Range');
+				var range1 = range.split("/");
+				var range2 = range1[0].split("-");
+				if (range1[1] != "*") {
+					grid.size = Number(range1[1]);
+				} else {
+					grid.size = Number(range2[1]) + grid.pageSize * 2;
+				}
+				var vaadinItems = new Array();
+				function checkChar(characteristic) {
+					return characteristic.name == "username";
+				}
+				for (var index in request.response) {
+					var newRecord = new Object();
+					newRecord.id = request.response[index].id;
+					if(request.response[index].product) {
+						newRecord.product = request.response[index].product;
+					} else {
+						newRecord.product = document.getElementById("subscriberAdd").productId;
+					}
+					if(request.response[index].attributes) {
+						request.response[index].attributes.forEach(
+							function(attrObj) {
+								newRecord[attrObj.name] = attrObj.value;
+							}
+						);
+					}
+					function checkPassword(pass) {
+						return pass.name == "servicePassword";
+					}
+					var indexPass = request.response[index].serviceCharacteristic.findIndex(checkPassword);
+					if(indexPass != -1) {
+						newRecord.password = request.response[index].serviceCharacteristic[indexPass].value;
+					}
+					newRecord.enabled = request.response[index].isServiceEnabled;
+					function checkMultisession(multi) {
+						return multi.name == "multiSession";
+					}
+					var indexMulti = request.response[index].serviceCharacteristic.findIndex(checkMultisession);
+					if(indexMulti != -1) {
+						newRecord.multisession = request.response[index].serviceCharacteristic[indexMulti].value;
+					}
+					vaadinItems[index] = newRecord;
+				}
+			callback(vaadinItems);
+			} else {
+				grid.size = 0;
+				callback([]);
+			}
+		};
+		var handleAjaxError = function(error) {
+			serviceList.etag = null;
+			var toast = document.body.querySelector('sig-app').shadowRoot.getElementById('restError');
+			toast.text = error;
+			toast.open();
+			if(!grid.size) {
+				grid.size = 0;
+			}
+			callback([]);
+		}
+		if (ajax.loading) {
+			ajax.lastRequest.completes.then(function(request) {
+				var startRange = params.page * params.pageSize + 1;
+				var endRange = startRange + params.pageSize - 1;
+				ajax.headers['Range'] = "items=" + startRange + "-" + endRange;
+				if (serviceList.etag && params.page > 0) {
+					ajax.headers['If-Range'] = serviceList.etag;
+				} else {
+					delete ajax.headers['If-Range'];
+				}
+				return ajax.generateRequest().completes;
+			}, handleAjaxError).then(handleAjaxResponse, handleAjaxError);
+		} else {
+			var startRange = params.page * params.pageSize + 1;
+			var endRange = startRange + params.pageSize - 1;
+			ajax.headers['Range'] = "items=" + startRange + "-" + endRange;
+			if (serviceList.etag && params.page > 0) {
+				ajax.headers['If-Range'] = serviceList.etag;
+			} else {
+				delete ajax.headers['If-Range'];
+			}
+			ajax.generateRequest().completes.then(handleAjaxResponse, handleAjaxError);
+		}
+	}
+
+	_filterChanged(filter) {
+		this.etag = null;
+		var grid = this.shadowRoot.getElementById('subscriberGrid');
+		grid.size = 0;
+	}
+
+	showAddModal(event) {
+		document.body.querySelector('sig-app').shadowRoot.querySelector('sig-sub-add').shadowRoot.getElementById('addServiceModal').open();
+	}
+}
+
+window.customElements.define('sig-sub-list', subList);
