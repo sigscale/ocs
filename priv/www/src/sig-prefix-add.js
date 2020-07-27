0836b04338613de37efaa49ad92b9275d955eaa0
Krishmanl <krishmanl@sigscale.com>
2020-07-27 11:40:41 +0000

    modernize the prefix-add module

--- priv/www/src/sig-prefix-add.js
+++ priv/www/src/sig-prefix-add.js
@@ -1,161 +1,158 @@
-<!--  vim: set ts=3:  -->
-<link rel="import" href="polymer/polymer.html">
-<link rel="import" href="i18n-msg/i18n-msg.html">
-<link rel="import" href="i18n-msg/i18n-msg-behavior.html">
-<link rel="import" href="iron-ajax/iron-ajax.html">
-<link rel="import" href="paper-dialog/paper-dialog.html">
-<link rel="import" href="paper-dropdown-menu/paper-dropdown-menu.html">
-<link rel="import" href="paper-listbox/paper-listbox.html">
-<link rel="import" href="paper-toolbar/paper-toolbar.html">
-<link rel="import" href="paper-input/paper-input.html">
-<link rel="import" href="paper-button/paper-button.html">
-<link rel="import" href="paper-tooltip/paper-tooltip.html">
-<link rel="import" href="paper-styles/color.html">
-<link rel="import" href="paper-menu-button/paper-menu-button.html" />
-<link rel="import" href="paper-icon-button/paper-icon-button.html">
-<link rel="import" href="iron-icon/iron-icon.html">
-<link rel="import" href="iron-icons/iron-icons.html">
+/**
+ * @license
+ * Copyright (c) 2020 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
 
-<dom-module id="sig-prefix-add">
-	<template>
-		<style is="custom-style">
-			paper-dialog {
-				overflow: auto;
-			}
-			paper-input {
-				--paper-input-container-focus-color: var(--paper-yellow-900);
-			}
-			paper-toolbar{
-				margin-top: 0px;
-				color: white;
-				background-color: #bc5100;
-			}
-			.add-button {
-				background-color: var(--paper-lime-a700);
-				color: black;
-				width: 8em;
-			}
-			.cancel-button {
-				color: black;
-			}
-		</style>
-		<paper-dialog id="addPrefixModal" modal>
-			<paper-toolbar>
-				<h2>[[i18n.addPrefix]]</h2>
-			</paper-toolbar>
-			<div>
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import '@polymer/iron-ajax/iron-ajax.js';
+import '@polymer/paper-dialog/paper-dialog.js';
+import '@polymer/app-layout/app-toolbar/app-toolbar.js';
+import '@polymer/paper-progress/paper-progress.js';
+import '@polymer/paper-dropdown-menu/paper-dropdown-menu.js';
+import '@polymer/paper-input/paper-input.js';
+import '@polymer/paper-input/paper-textarea.js';
+import '@polymer/paper-button/paper-button.js';
+import '@polymer/paper-listbox/paper-listbox.js';
+import '@polymer/paper-item/paper-item.js'
+import '@polymer/paper-checkbox/paper-checkbox.js'
+import './style-element.js';
+
+class prefixAdd extends PolymerElement {
+	static get template() {
+		return html`
+			<style include="style-element"></style>
+			<paper-dialog class="dialog" id="addPrefixModal" modal>
+				<app-toolbar>
+					<h2>Add Prefix</h2>
+				</app-toolbar>
+				<paper-progress
+						indeterminate
+						class="slow red"
+						disabled="{{!loading}}">
+				</paper-progress>
 				<paper-input
-					id="addPrefix"
-					type="number"
-					name="Prefix"
-					allowed-pattern="[0-9]"
-					label="[[i18n.prefix]]">
+						id="addPrefix"
+						type="number"
+						name="Prefix"
+						allowed-pattern="[0-9]"
+						label="Prefix"
+						value="{{addPre}}">
 				</paper-input>
-			</div>
-			<div>
 				<paper-input
-					id="addDesc"
-					name="Description"
-					label="[[i18n.des]]">
+						id="addDesc"
+						name="Description"
+						label="Description"
+						value="{{addPreDesc}}">
 				</paper-input>
-			</div>
-			<div>
 				<paper-input
-					id="addRateRow"
-					type="number"
-					name="PriceName"
-					label="[[i18n.rate]]">
+						id="addRateRow"
+						type="number"
+						name="PriceName"
+						label="Rate"
+						value="{{addPreRate}}">
 				</paper-input>
-			</div>
-			<div class="buttons">
-				<paper-button dialog-confirm
-						raised
-						on-tap="_tableRow"
-						class="add-button">
-					<i18n-msg msgid="submit">
-							Submit
-					</i18n-msg>
-				</paper-button>
-				<paper-button
-						class="cancel-button"
-						on-tap="_cancel"
-						dialog-dismiss>
-					<i18n-msg msgid="cancel">
-							Cancel
-					</i18n-msg>
-				</paper-button>
-			</div>
-		</paper-dialog>
-		<paper-toast
-			id="addTableRowToastError">
-		</paper-toast>
-		<iron-ajax
-			id="addTableRowAjax"
-			url="/catalogManagement/v2/pla"
-			method = "POST"
-			content-type="application/json"
-			on-loading-changed="_onLoadingChanged"
-			on-response="_addTableResponse"
-			on-error="_addTableError">
-		</iron-ajax>
-	</template>
-	<script>
-		Polymer ({
-			is: 'sig-prefix-add',
-			behaviors: [i18nMsgBehavior],
-			_tableRow: function(event) {
-				var table = document.getElementById('prefixList').table;
-				var rowName = new Object();
-				rowName.id = this.$.addPrefix.value;
-				rowName.href = "/resourceInventoryManagement/v1/logicalResource/" + table + "/" + rowName.id;
-				var resource = new Array();
-				var resPre = new Object();
-				resPre.name = "prefix";
-				seqNum = 1;
-				value = this.$.addPrefix.value;
-				resPre.value = {seqNum, value};
-				resource.push(resPre);
-				var resDes = new Object();
-				resDes.name = "description";
-				seqNum = 2;
-				value = this.$.addDesc.value;
-				resDes.value = {seqNum, value};
-				resource.push(resDes);
-				var resRate = new Object();
-				resRate.name = "rate";
-				seqNum = 3;
-				value = this.$.addRateRow.value;
-				resRate.value = {seqNum, value};
-				resource.push(resRate);
-				rowName.resourceCharacteristic = resource;
-				var ajax = this.$.addTableRowAjax;
-				ajax.url = "/resourceInventoryManagement/v1/logicalResource/" + table
-				ajax.body = rowName;
-				ajax.generateRequest();
-				this.$.addPrefix.value = null;
-				this.$.addDesc.value = null;
-				this.$.addRateRow.value = null;
+				<div class="buttons">
+					<paper-button dialog-confirm
+							raised
+							on-tap="_tableRow"
+							class="add-button">
+						Submit
+					</paper-button>
+					<paper-button
+							class="cancel-button"
+							on-tap="_cancel"
+							dialog-dismiss>
+						Cancel
+					</paper-button>
+				</div>
+			</paper-dialog>
+			<paper-toast
+					id="addTableRowToastError">
+			</paper-toast>
+			<iron-ajax
+					id="addTableRowAjax"
+					url="/catalogManagement/v2/pla"
+					method = "POST"
+					content-type="application/json"
+					on-loading-changed="_onLoadingChanged"
+					on-response="_addTableResponse"
+					on-error="_addTableError">
+			</iron-ajax>
+		`;
+	}
+	static get properties() {
+		return {
+				loading: {
+				type: Boolean,
+				value: false
 			},
-			_addTableResponse: function(event) {
-				this.$.addPrefixModal.close();
-				document.getElementById("prefixGrid").clearCache();
+			addPre: {
+				type: String,
 			},
-			_addTableError: function(event) {
-				this.$.addTableRowToastError.text = event.detail.request.xhr.statusText;
-				this.$.addTableRowToastError.open();
+			addPreDesc: {
+				type: String,
 			},
-			_cancel: function() {
-				this.$.addPrefix.value = null;
-				this.$.addDesc.value = null;
-				this.$.addRateRow.value = null;
-			},
-			_onLoadingChanged: function(event) {
-				if (this.$.addTableRowAjax.loading) {
-					document.getElementById("progress").disabled = false;
-				} else {
-					document.getElementById("progress").disabled = true;
-				}
+			addPreRate: {
+				type: String,
 			}
-		});
-	</script>
-</dom-module>
+		}
+	}
+
+	_tableRow(event) {
+//		var table = document.getElementById('prefixList').table;
+		var table = document.body.querySelector('sig-app').shadowRoot.getElementById('prefixList');
+		var rowName = new Object();
+		rowName.id = this.addPre;
+		rowName.href = "/resourceInventoryManagement/v1/logicalResource/" + table + "/" + rowName.id;
+		var resource = new Array();
+		var resPre = new Object();
+		resPre.name = "prefix";
+		seqNum = 1;
+		value = this.addPre;
+		resPre.value = {seqNum, value};
+		resource.push(resPre);
+		var resDes = new Object();
+		resDes.name = "description";
+		seqNum = 2;
+		value = this.addPreDesc;
+		resDes.value = {seqNum, value};
+		resource.push(resDes);
+		var resRate = new Object();
+		resRate.name = "rate";
+		seqNum = 3;
+		value = this.addPreRate;
+		resRate.value = {seqNum, value};
+		resource.push(resRate);
+		rowName.resourceCharacteristic = resource;
+		var ajax = this.$.addTableRowAjax;
+		ajax.url = "/resourceInventoryManagement/v1/logicalResource/" + table
+		ajax.body = rowName;
+		ajax.generateRequest();
+		this.addPre = null;
+		this.addPreDesc = null;
+		this.$.addRateRow.value = null;
+	}
+
+	_addTableResponse(event) {
+		this.$.addPrefixModal.close();
+		document.body.querySelector('sig-app').shadowRoot.getElementById('prefixList').shadowRoot.getElementById('prefixGrid').clearCache();
+	}
+
+	_addTableError(event) {
+		this.$.addTableRowToastError.text = event.detail.request.xhr.statusText;
+		this.$.addTableRowToastError.open();
+	}
+
+	_cancel() {
+		this.addPre = null;
+		this.addDesc = null;
+		this.addRateRow = null;
+	}
+}
+
+window.customElements.define('sig-prefix-add', prefixAdd);
