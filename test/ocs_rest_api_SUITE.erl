1b1540d4dea031b3c5690b31ea14b5c869c565b3
kusalfernando <kusal@sigscale.com>
2020-07-31 10:05:00 +0000

    correct the indentation

--- test/ocs_rest_api_SUITE.erl
+++ test/ocs_rest_api_SUITE.erl
@@ -2528,30 +2528,30 @@ update_client_attributes_json_patch(Config) ->
 	ok = ssl:close(SslSock).
 
 post_hub() ->
-   [{userdata, [{doc, "Register hub listener"}]}].
+	[{userdata, [{doc, "Register hub listener"}]}].
 
 post_hub(Config) ->
-   HostUrl = ?config(host_url, Config),
-   PathHub = ?PathBalanceHub,
-   CollectionUrl = HostUrl ++ PathHub,
-   Callback = "http://in.listener.com",
-   RequestBody = "{\n"
-         ++ "\t\"callback\": \"" ++ Callback ++ "\",\n"
-         ++ "}\n",
-   ContentType = "application/json",
-   Accept = {"accept", "application/json"},
-   Request = {CollectionUrl, [Accept, auth_header()], ContentType, RequestBody},
-   {ok, Result} = httpc:request(post, Request, [], []),
-   {{"HTTP/1.1", 201, _Created}, Headers, ResponseBody} = Result,
-   {_, "application/json"} = lists:keyfind("content-type", 1, Headers),
-   ContentLength = integer_to_list(length(ResponseBody)),
-   {_, ContentLength} = lists:keyfind("content-length", 1, Headers),
-   {_, Location} = lists:keyfind("location", 1, Headers),
-   Id = string:substr(Location, string:rstr(Location, PathHub) + length(PathHub)),
-   {struct, HubList} = mochijson:decode(ResponseBody),
-   {_, Callback} = lists:keyfind("callback", 1, HubList),
-   {_, Id} = lists:keyfind("id", 1, HubList),
-   {_, null} = lists:keyfind("query", 1, HubList).
+	HostUrl = ?config(host_url, Config),
+	PathHub = ?PathBalanceHub,
+	CollectionUrl = HostUrl ++ PathHub,
+	Callback = "http://in.listener.com",
+	RequestBody = "{\n"
+			++ "\t\"callback\": \"" ++ Callback ++ "\",\n"
+			++ "}\n",
+	ContentType = "application/json",
+	Accept = {"accept", "application/json"},
+	Request = {CollectionUrl, [Accept, auth_header()], ContentType, RequestBody},
+	{ok, Result} = httpc:request(post, Request, [], []),
+	{{"HTTP/1.1", 201, _Created}, Headers, ResponseBody} = Result,
+	{_, "application/json"} = lists:keyfind("content-type", 1, Headers),
+	ContentLength = integer_to_list(length(ResponseBody)),
+	{_, ContentLength} = lists:keyfind("content-length", 1, Headers),
+	{_, Location} = lists:keyfind("location", 1, Headers),
+	Id = string:substr(Location, string:rstr(Location, PathHub) + length(PathHub)),
+	{struct, HubList} = mochijson:decode(ResponseBody),
+	{_, Callback} = lists:keyfind("callback", 1, HubList),
+	{_, Id} = lists:keyfind("id", 1, HubList),
+	{_, null} = lists:keyfind("query", 1, HubList).
 
 notify_create_bucket() ->
 	[{userdata, [{doc, "Receive balance creation notification."}]}].
