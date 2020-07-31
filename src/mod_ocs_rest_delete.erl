a2d51ac632ef7efeac3c0ef729a1ef80f56de42c
kusalfernando <kusal@sigscale.com>
2020-07-31 11:58:14 +0000

    rest api for unregistering an existing listener

--- src/mod_ocs_rest_delete.erl
+++ src/mod_ocs_rest_delete.erl
@@ -83,6 +83,8 @@ do_delete(Resource, ModData, ["serviceInventoryManagement", "v2", "service", Ide
 	do_response(ModData, Resource:delete_inventory(Identity));
 do_delete(Resource, ModData, ["resourceInventoryManagement", "v1", "logicalResource", Table, Identity]) ->
 	do_response(ModData, Resource:delete_resource_inventory(Table, Identity));
+do_delete(Resource, ModData, ["balanceManagement", "v1", "hub", Identity]) ->
+	do_response(ModData, Resource:delete_hub(Identity));
 do_delete(_Resource, #mod{data = Data} = _ModData, _) ->
 	Response = "<h2>HTTP Error 404 - Not Found</h2>",
 	{proceed, [{response, {404, Response}} | Data]}.
