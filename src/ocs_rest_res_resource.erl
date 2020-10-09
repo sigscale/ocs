80638c4a680c77bbc6370151c4eaf5929fc158a9
Vance Shipley <vances@sigscale.org>
2020-10-09 19:19:48 +0800

    edoc function description corrections

--- src/ocs_rest_res_resource.erl
+++ src/ocs_rest_res_resource.erl
@@ -107,7 +107,7 @@ get_resource_category(_) ->
 	Headers	:: [tuple()],
 	Body		:: iolist(),
 	Status	:: 400 | 404 | 500.
-%% @doc Respond to `GET /catalogManegment/v2/resourceCategory'.
+%% @doc Respond to `GET /catalogManagement/v2/resourceCategory'.
 %% 	Retrieve all Resource categories.
 get_resource_categories([] = _Query) ->
 	Headers = [{content_type, "application/json"}],
@@ -136,7 +136,7 @@ get_resource_candidate(_) ->
 	Headers	:: [tuple()],
 	Body		:: iolist(),
 	Status	:: 400 | 404 | 500.
-%% @doc Respond to `GET /catalogManegment/v2/resourceCandidate'.
+%% @doc Respond to `GET /catalogManagement/v2/resourceCandidate'.
 %% 	Retrieve all Resource candidate.
 get_resource_candidates([] = _Query) ->
 	Headers = [{content_type, "application/json"}],
@@ -256,8 +256,8 @@ add_resource_inventory(Table, ReqData) ->
 	Headers  :: [tuple()],
 	Body     :: iolist(),
 	Status   :: 400 | 500 .
-%% @doc Respond to `POST /catalogManagement/v2/pla'.
-%%    Add a new Product Offering.
+%% @doc Respond to `POST resourceCatalogManagement/v2/pla'.
+%%    Add a new Pricing Logic Algorithm (PLA).
 add_pla(ReqData) ->
 	try
 		case ocs:add_pla(ocs_rest_res_resource:pla(mochijson:decode(ReqData))) of
@@ -292,8 +292,8 @@ add_pla(ReqData) ->
 		Headers	:: [tuple()],
 		Body		:: iolist(),
 		Status	:: 400 | 404 | 412 | 500 .
-%% @doc Respond to `PATCH /catalogManagement/v2/pla/{id}'.
-%% 	Update a pricing logic algorithm using JSON patch method
+%% @doc Respond to `PATCH resourceCatalogManagement/v2/pla/{id}'.
+%% 	Update a Pricing Logic Algorithm (PLA) using JSON patch method.
 patch_pla(Id, Etag, ReqData) ->
 	try
 		Etag1 = case Etag of
@@ -353,8 +353,8 @@ patch_pla(Id, Etag, ReqData) ->
 		Id :: string(),
 		Result :: {ok, Headers :: [tuple()], Body :: iolist()}
 				| {error, ErrorCode :: integer()} .
-%% @doc Respond to `DELETE /catalogManagement/v2/pla/{id}'
-%% 	request to remove a `Pla'.
+%% @doc Respond to `DELETE /resourceCatalogManagement/v2/pla/{id}'
+%%    request to remove a Pricing Logic Algorithm (PLA).
 delete_pla(Id) when is_list(Id) ->
 	case catch list_to_existing_atom(Id) of
 		{'EXIT', _Reason} ->
@@ -370,8 +370,8 @@ delete_pla(Id) when is_list(Id) ->
 	Headers :: [tuple()],
 	Body :: iolist(),
 	Status :: 400 | 404 | 500.
-%% @doc Respond to `GET /catalogManagement/v2/pla/{id}'.
-%%    Retrieve a pricing logic algorothm.
+%% @doc Respond to `GET /resourceCatalogManagement/v2/pla/{id}'.
+%%    Retrieve a Pricing Logic Algorothm (PLA).
 get_pla(ID) ->
 	try
 		case ocs:find_pla(ID) of
@@ -400,8 +400,8 @@ get_pla(ID) ->
 	Headers :: [tuple()],
 	Body :: iolist(),
 	Status :: 400 | 404 | 412 | 500 .
-%% @doc Respond to `GET /catalogManagement/v2/pla'.
-%%    Retrieve all pricing logic algorithms.
+%% @doc Respond to `GET /resourceCatalogManagement/v2/pla'.
+%%    Retrieve all Pricing Logic Algorithms (PLA).
 get_plas(_Query, _Headers) ->
 	try
 		case ocs:get_plas() of
@@ -433,7 +433,7 @@ get_plas(_Query, _Headers) ->
 	Body		:: iolist(),
 	Status	:: 400 | 404 | 500.
 %% @doc Respond to `GET /resourceCatalogManagement/v2/plaSpecification'.
-%% 	Retrieve all pricing logic algorithm specifications.
+%% 	Retrieve all Pricing Logic Algorithm (PLA) specifications.
 get_pla_specs([] = _Query) ->
 	Headers = [{content_type, "application/json"}],
 	Object = {array, [spec_pla_once(), spec_pla_recurring(),
@@ -454,7 +454,7 @@ get_pla_specs(_Query) ->
 		Body		:: iolist(),
 		Status	:: 400 | 404 | 500 .
 %% @doc Respond to `PATCH /resourceInventoryManagement/v1/logicalResource/{table}/{id}'.
-%% 	Update a table row using JSON patch method
+%% 	Update a table row using JSON patch method.
 patch_resource_inventory(Table, Id, _Etag, ReqData) ->
 	try
 		Table1 = list_to_existing_atom(Table),
@@ -495,7 +495,7 @@ patch_resource_inventory(Table, Id, _Etag, ReqData) ->
       Result :: {ok, Headers :: [tuple()], Body :: iolist()}
             | {error, ErrorCode :: integer()} .
 %% @doc Respond to `DELETE /resourceInventoryManagement/v1/logicalResource/{table}/{id}''
-%%    request to remove a `Table row'.
+%%    request to remove a table row.
 delete_resource_inventory(Table, Id) ->
 	try
 		Name = list_to_existing_atom(Table),
