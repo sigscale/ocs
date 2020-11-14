e0b92ce111c11338a354c0cdc96db659c5d8d50e
Vance Shipley <vances@sigscale.org>
2020-11-14 12:01:13 +0000

    correct sw-precache configuration; use generated service-worker

--- priv/www/sw-precache-config.js
+++ priv/www/sw-precache-config.js
@@ -1,21 +1,11 @@
-/**
- * @license
- * Copyright (c) 2020 The Polymer Project Authors. All rights reserved.
- * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
- * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
- * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
- * Code distributed by Google as part of the polymer project is also
- * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
- */
-
 module.exports = {
-  staticFileGlobs: [
-    '/index.html',
-    '/node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle.js',
-    '/images/*'
-    'src/*.js',
-    'manifest.json'
-  ],
-  navigateFallback: '/index.html',
-  navigateFallbackWhitelist: [/^(?!.*\.js$|\/data\/).*/]
+	staticFileGlobs: [
+		'/index.html',
+		'/manifest.json',
+		'/node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle.js',
+		'/src/*.js',
+		'/images/*'
+	],
+	navigateFallback: '/index.html',
+	navigateFallbackWhitelist: [/^(?!.*\.js$|\/data\/).*/]
 }
