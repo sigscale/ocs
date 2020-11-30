3fd6faf79b2ae4be50c01bf4c2281613373e348c
Vance Shipley <vances@sigscale.org>
2020-11-30 16:51:39 +0000

    dispatch s6b requests to pwg fsm

--- src/ocs_diameter_auth_port_server.erl
+++ src/ocs_diameter_auth_port_server.erl
@@ -46,6 +46,7 @@
 -include("diameter_gen_eap_application_rfc4072.hrl").
 -include("diameter_gen_3gpp_sta_application.hrl").
 -include("diameter_gen_3gpp_swm_application.hrl").
+-include("diameter_gen_3gpp_s6b_application.hrl").
 -include("ocs_eap_codec.hrl").
 -include("ocs.hrl").
 
@@ -60,6 +61,7 @@
 		aka_sup :: undefined | pid(),
 		akap_sup :: undefined | pid(),
 		pwd_sup :: undefined | pid(),
+		pgw_sup :: undefined | pid(),
 		cb_fsms = gb_trees:empty() :: gb_trees:tree(
 				Key :: (AuthFsm :: pid()), Value :: (CbProc :: pid())),
 		handlers = gb_trees:empty() :: gb_trees:tree(
@@ -173,8 +175,10 @@ handle_info(timeout, #state{auth_port_sup = AuthPortSup} = State) ->
 	{_, TtlsSup, _, _} = lists:keyfind(ocs_eap_ttls_fsm_sup_sup, 1, Children),
 	{_, AkaSup, _, _} = lists:keyfind(ocs_eap_aka_fsm_sup_sup, 1, Children),
 	{_, AkapSup, _, _} = lists:keyfind(ocs_eap_akap_fsm_sup_sup, 1, Children),
-	NewState = State#state{simple_auth_sup = SimpleAuthSup, pwd_sup = PwdSup,
-			ttls_sup = TtlsSup, aka_sup = AkaSup, akap_sup = AkapSup},
+	{_, PgwSup, _, _} = lists:keyfind(ocs_pgw_fsm_sup, 1, Children),
+	NewState = State#state{simple_auth_sup = SimpleAuthSup,
+			pwd_sup = PwdSup, ttls_sup = TtlsSup,
+			aka_sup = AkaSup, akap_sup = AkapSup, pgw_sup = PgwSup},
 	{noreply, NewState};
 handle_info({'EXIT', Fsm, {shutdown, SessionId}},
 		#state{handlers = Handlers} = State) ->
@@ -247,12 +251,14 @@ code_change(_OldVsn, State, _Extra) ->
 		PasswordReq :: boolean(),
 		Trusted :: boolean(),
 		Request :: #diameter_nas_app_AAR{} | #diameter_nas_app_STR{}
-				| #diameter_eap_app_DER{} | #'3gpp_sta_DER'{} | #'3gpp_swm_DER'{},
+				| #diameter_eap_app_DER{} | #'3gpp_sta_DER'{}
+				| #'3gpp_swm_DER'{} | #'3gpp_s6b_AAR'{},
 		CbProc :: {pid(), term()},
 		State :: state(),
 		Reply :: {reply, Answer, State} | {noreply, State},
 		Answer ::#diameter_nas_app_AAA{} | #diameter_nas_app_STA{}
-				| #diameter_eap_app_DEA{} | #'3gpp_sta_DEA'{} | #'3gpp_swm_DEA'{}.
+				| #diameter_eap_app_DEA{} | #'3gpp_sta_DEA'{}
+				| #'3gpp_swm_DEA'{} | #'3gpp_s6b_AAA'{}.
 %% @doc Generate appropriate DIAMETER answer.
 %% @hidden
 request(Caps, Address, Port, none, PasswordReq, Trusted, Request, CbProc, State)
@@ -305,6 +311,24 @@ request(Caps, _Address, _Port, none, _PasswordReq, _Trusted, Request, _CbProc, S
 	catch
 		_:_ ->
 			Answer = #diameter_nas_app_STA{'Session-Id' = SessionId,
+					'Result-Code' = ?'DIAMETER_BASE_RESULT-CODE_UNABLE_TO_COMPLY',
+					'Origin-Host' = OHost, 'Origin-Realm' = ORealm},
+			{reply, Answer, State}
+	end;
+request(Caps, ClientAddress, ClientPort, none, _PasswordReq, _Trusted,
+		#'3gpp_s6b_AAR'{'Session-Id' = SessionId} = Request, _CbProc,
+		#state{address = ServerAddress, port = ServerPort,
+		pgw_sup = PgwSup} = State) ->
+	#diameter_caps{origin_host = {OHost, DHost}, origin_realm = {ORealm, DRealm}} = Caps,
+	ChildSpec = [ServerAddress, ServerPort, ClientAddress, ClientPort,
+		SessionId, OHost, ORealm, DHost, DRealm, Request],
+	case supervisor:start_child(PgwSup, ChildSpec) of
+		{ok, PgwFsm} ->
+			{noreply, State};
+		{error, Reason} ->
+			error_logger:error_report(["Error starting session handler",
+					{error, Reason}, {supervisor, PgwSup}, {session_id, SessionId}]),
+			Answer = #'3gpp_s6b_AAA'{'Session-Id' = SessionId,
 					'Result-Code' = ?'DIAMETER_BASE_RESULT-CODE_UNABLE_TO_COMPLY',
 					'Origin-Host' = OHost, 'Origin-Realm' = ORealm},
 			{reply, Answer, State}
