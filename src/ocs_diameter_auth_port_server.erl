b5f93a2d34a4c0bc7dd101ff61cc49a1d4063b56
Vance Shipley <vances@sigscale.org>
2020-12-02 14:56:12 +0000

    start deregister handler fsm from diameter swx callback

--- src/ocs_diameter_auth_port_server.erl
+++ src/ocs_diameter_auth_port_server.erl
@@ -46,7 +46,6 @@
 -include("diameter_gen_eap_application_rfc4072.hrl").
 -include("diameter_gen_3gpp_sta_application.hrl").
 -include("diameter_gen_3gpp_swm_application.hrl").
--include("diameter_gen_3gpp_swx_application.hrl").
 -include("ocs_eap_codec.hrl").
 -include("ocs.hrl").
 
@@ -316,24 +315,6 @@ request(Caps, _Address, _Port, none, _PasswordReq, _Trusted, Request, _CbProc, S
 	catch
 		_:_ ->
 			Answer = #diameter_nas_app_STA{'Session-Id' = SessionId,
-					'Result-Code' = ?'DIAMETER_BASE_RESULT-CODE_UNABLE_TO_COMPLY',
-					'Origin-Host' = OHost, 'Origin-Realm' = ORealm},
-			{reply, Answer, State}
-	end;
-request(Caps, ClientAddress, ClientPort, none, _PasswordReq, _Trusted,
-		#'3gpp_swx_RTR'{'Session-Id' = SessionId} = Request, _CbProc,
-		#state{address = ServerAddress, port = ServerPort,
-		dereg_sup = Sup} = State) ->
-	#diameter_caps{origin_host = {OHost, DHost}, origin_realm = {ORealm, DRealm}} = Caps,
-	ChildSpec = [[ServerAddress, ServerPort, ClientAddress, ClientPort,
-		SessionId, OHost, ORealm, DHost, DRealm, Request], []],
-	case supervisor:start_child(Sup, ChildSpec) of
-		{ok, _Fsm} ->
-			{noreply, State};
-		{error, Reason} ->
-			error_logger:error_report(["Error starting SWx UE deregister handler",
-					{error, Reason}, {supervisor, Sup}, {session_id, SessionId}]),
-			Answer = #'3gpp_swx_RTA'{'Session-Id' = SessionId,
 					'Result-Code' = ?'DIAMETER_BASE_RESULT-CODE_UNABLE_TO_COMPLY',
 					'Origin-Host' = OHost, 'Origin-Realm' = ORealm},
 			{reply, Answer, State}
