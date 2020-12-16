46d6e57a4f9e2817fa3074a61cdf5013cf8bbe8e
Vance Shipley <vances@sigscale.org>
2020-12-16 12:00:50 +0000

    remove session on deregister failure

--- src/ocs_terminate_fsm.erl
+++ src/ocs_terminate_fsm.erl
@@ -168,6 +168,7 @@ idle(Request, From, #statedata{session_id = SessionId} = StateData)
 				[#session{imsi = IMSI, hss_realm = undefined}] ->
 					mnesia:delete(session, SessionId, write);
 				[#session{imsi = IMSI, hss_realm = HR, hss_host = HH}] ->
+					mnesia:delete(session, SessionId, write),
 					{HR, HH};
 				[] ->
 					not_found
@@ -246,7 +247,7 @@ deregister({ok, #'3gpp_swx_SAA'{'Result-Code' = [ResultCode]} = _Answer},
 			{session, SessionId}, {result, ResultCode}]),
 	ResultCode = ?'DIAMETER_BASE_RESULT-CODE_UNABLE_TO_COMPLY',
 	gen_fsm:reply(Caller, response(ResultCode, StateData)),
-	{stop, shutdown, StateData};
+	deregister1(StateData);
 deregister({ok, #'3gpp_swx_SAA'{'Experimental-Result'
 		= [#'3gpp_Experimental-Result'{'Experimental-Result-Code' = ResultCode1}]}},
 		#statedata{session_id = SessionId, imsi = IMSI, identity = Identity,
@@ -260,7 +261,7 @@ deregister({ok, #'3gpp_swx_SAA'{'Experimental-Result'
 			{session, SessionId}, {result, ResultCode1}]),
 	ResultCode2 = ?'DIAMETER_BASE_RESULT-CODE_UNABLE_TO_COMPLY',
 	gen_fsm:reply(Caller, response(ResultCode2, StateData)),
-	{stop, shutdown, StateData}.
+	deregister1(StateData).
 %% @hidden
 deregister1(#statedata{session_id = SessionId} = StateData) ->
 	F = fun() -> mnesia:delete(session, SessionId, write) end,
