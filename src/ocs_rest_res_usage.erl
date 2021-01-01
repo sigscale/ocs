09699e6798ecd7b5d591e98d69e786d259cc8f63
refath <refath@sigscale.com>
2021-01-01 13:39:38 +0000

    typos

--- src/ocs_rest_res_usage.erl
+++ src/ocs_rest_res_usage.erl
@@ -2843,7 +2843,6 @@ query_page(PageServer, Etag, Query, Filters, Start, End) ->
 	end.
 %% @hidden
 query_page1(PageServer, Etag, Decoder, Filters, Start, End) ->
-erlang:display({?MODULE, ?LINE, Filters}),
 	case gen_server:call(PageServer, {Start, End}, infinity) of
 		{error, Status} ->
 			{error, Status};
@@ -3212,13 +3211,13 @@ characteristic({"name", exact, "msisdn"}, {"value", Op, UserName},
 			Prefix2 = integer_to_binary(I + 1),
 			ReqAttrs2 = [{CCR#'3gpp_ro_CCR'{'Subscription-Id'
 					= [#'3gpp_ro_Subscription-Id'{'Subscription-Id-Data'
-					= VarMatch, _ = '_'}]},
+					= VarMatch, _ = '_'} | '_']},
 					[{'=<', Prefix, VarMatch}, {'>', Prefix2, VarMatch}]}];
 		_ when Op == exact ->
 			Prefix = list_to_binary(UserName),
 			ReqAttrs2 = [{CCR#'3gpp_ro_CCR'{'Subscription-Id'
 					= [#'3gpp_ro_Subscription-Id'{'Subscription-Id-Data'
-					= Prefix, _ = '_'}]}, []}]
+					= Prefix, _ = '_'} | '_']}, []}]
 	end,
 	characteristic(T, diameter, Types, ReqAttrs2, RespAttrs, N + 1);
 characteristic({"name", exact, "imsi"}, {"value", Op, UserName},
