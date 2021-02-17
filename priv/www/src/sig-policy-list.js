e8ca3b5371affdb152dc1bcfdf9973cb0c2321f3
Krishmanl <krishmanl@sigscale.com>
2021-02-17 05:06:47 +0000

    create list of policy tables and view and policy row view

--- priv/www/src/sig-policy-list.js
+++ priv/www/src/sig-policy-list.js
@@ -30,41 +30,82 @@ class policyList extends PolymerElement {
 					theme="no-border">
 				<vaadin-grid-column>
 					<template class="header">
-						Id
-					</template>
-					<template>[[item.id]]</template>
-				</vaadin-grid-column>
-				<vaadin-grid-column>
-					<template class="header">
 						Name
 					</template>
 					<template>[[item.name]]</template>
 				</vaadin-grid-column>
 				<vaadin-grid-column>
 					<template class="header">
-						Description
+						QOS
 					</template>
-					<template>[[item.description]]</template>
+					<template>[[item.qos]]</template>
 				</vaadin-grid-column>
 				<vaadin-grid-column>
 					<template class="header">
-						Category
+						Charging Rule
 					</template>
-					<template>[[item.category]]</template>
+					<template>[[item.chargingRule]]</template>
 				</vaadin-grid-column>
 				<vaadin-grid-column>
 					<template class="header">
-						Type
+						Flow Up
 					</template>
-					<template>[[item.type]]</template>
+					<template>[[item.flowUp]]</template>
 				</vaadin-grid-column>
 				<vaadin-grid-column>
 					<template class="header">
-						Specification
+						Flow Down
 					</template>
-					<template>[[item.resourceSpecification.name]]</template>
+					<template>[[item.flowDown]]</template>
+				</vaadin-grid-column>
+				<vaadin-grid-column>
+					<template class="header">
+						Precedence
+					</template>
+					<template>[[item.precedence]]</template>
 				</vaadin-grid-column>
 			</vaadin-grid>
+			<paper-dialog class="dialog" id="tableList">
+				<app-toolbar>
+					List of Tables
+				</app-toolbar>
+				<template is="dom-repeat" items="[[tables]]">
+					<paper-item
+							id="pagePolicy"
+							class="menuitem"
+							on-focused-changed="tableSelection">
+						<iron-icon icon ="icons:view-list" item-icon></iron-icon>
+							{{item.name}}
+					</paper-item>
+				</template>
+				<div class="buttons">
+					<paper-button
+							raised
+							id="tabOkButton"
+							disabled
+							on-tap="tableOk"
+							class="submit-button">
+						Ok
+					</paper-button>
+					<paper-button
+							dialog-dismiss
+							class="cancel-button">
+						Cancel
+					</paper-button>
+					<paper-button
+							raised
+							on-tap="tableAdd"
+							class="submit-button">
+						Add
+					</paper-button>
+					<paper-button
+							raised
+							on-tap="tableDelete"
+							class="delete-button">
+						Delete
+					</paper-button>
+				</div>
+			</paper-dialog>
 			<div class="add-button">
 				<paper-fab
 					icon="add"
@@ -74,8 +115,13 @@ class policyList extends PolymerElement {
 			<paper-toast
 				id="PolicyToast">
 			</paper-toast>
+			<iron-ajax id="getPolicyContentAjax"
+					on-response="_getPolicyContentResponse"
+					on-error="_getPolicyContentError">
+			</iron-ajax>
 			<iron-ajax id="getPolicyAjax"
-				url="/resourceInventoryManagement/v1/resource"
+				url="/resourceInventoryManagement/v1/resource?resourceSpecification.id=3"
+				on-response="_getPolicyResponse"
 				rejectWithRequest>
 			</iron-ajax>
 		`;
@@ -91,6 +137,14 @@ class policyList extends PolymerElement {
 				type: String,
 				value: null
 			},
+			tables: {
+				type: Array,
+				readOnly: true,
+				notify: true,
+				value: function() {
+					return []
+				}
+			},
 			activeItem: {
 				type: Object,
 				notify: true,
@@ -101,17 +155,47 @@ class policyList extends PolymerElement {
 
 	ready() {
 		super.ready();
-		var grid = this.shadowRoot.getElementById('policyGrid');
-		grid.dataProvider = this._getPolicy;
+		var ajax1 = document.body.querySelector('sig-app').shadowRoot.querySelector('sig-policy-list').shadowRoot.getElementById('getPolicyAjax');
+		ajax1.generateRequest();
+		this.$.tableList.open();
 	}
 
+	tableOk() {
+		var grid = this.shadowRoot.getElementById('policyGrid');
+		grid.dataProvider = this._getPolicy;
+		this.$.tableList.close();
+	} 
+
 	_activeItemChanged(item) {
-      if(item) {
-         this.$.policyGrid.selectedItems = item ? [item] : [];
+		if(item) {
+			this.$.policyGrid.selectedItems = item ? [item] : [];
 			document.body.querySelector('sig-app').shadowRoot.querySelector('sig-policy-update').shadowRoot.getElementById('updatePolicyModal').open();
-      } else {
-         this.$.policyGrid.selectedItems = [];
-      }
+		} else {
+			this.$.policyGrid.selectedItems = [];
+		}
+	}
+
+	_getPolicyResponse(event) {
+		var results = event.detail.xhr.response;
+		this.splice("tables", 0, this.tables.length)
+		for (var indexTable in results) {
+			var tableRecord = new Object();
+			tableRecord.name = results[indexTable].name;
+			tableRecord.id = results[indexTable].id;
+			tableRecord.href = results[indexTable].href;
+			tableRecord.description = results[indexTable].description;
+			tableRecord.plaSpecId = results[indexTable].plaSpecId;
+			this.push('tables', tableRecord);
+		}
+	}
+
+	tableSelection(e) {
+		if(e.model.item && e.model.item.id) {
+			document.body.querySelector('sig-app').shadowRoot.getElementById('policyList').table = e.model.item.name;
+			this.$.tabOkButton.disabled = false;
+		} else {
+			this.$.tabOkButton.disabled = true;
+		}
 	}
 
 	_getPolicy(params, callback) {
@@ -120,7 +204,9 @@ class policyList extends PolymerElement {
 			grid.size = 0;
 		}
 		var policyList = document.body.querySelector('sig-app').shadowRoot.querySelector('sig-policy-list');
-		var ajax = policyList.shadowRoot.getElementById('getPolicyAjax');
+		var ajax = policyList.shadowRoot.getElementById('getPolicyContentAjax');
+console.log(policyList.table);
+		ajax.url = "/resourceInventoryManagement/v1/resource?resourceSpecification.id=4&resourceRelationship.name=" + policyList.table;
 		var handleAjaxResponse = function(request) {
 			if(request) {
 				policyList.etag = request.xhr.getResponseHeader('ETag');
@@ -137,68 +223,42 @@ class policyList extends PolymerElement {
 					return characteristic.name == "name"
 				}
 				for (var index in request.response) {
-					var newRecord = new Object();
-					if(request.response[index].id) {
-						newRecord.id = request.response[index].id;
-					}
-					if(request.response[index].name) {
-						newRecord.name = request.response[index].name;
-					}
-					if(request.response[index].href) {
-						newRecord.href = request.response[index].href;
-					}
-					if(request.response[index].description) {
-						newRecord.description = request.response[index].description;
-					}
-					if(request.response[index].category) {
-						newRecord.category = request.response[index].category;
-					}
-					if(request.response[index]["@type"]) {
-						newRecord.type = request.response[index]["@type"];
-					}
-					if(request.response[index]["@baseType"]) {
-						newRecord.base = request.response[index]["@baseType"];
-					}
-					if(request.response[index]["@schemaLocation"]) {
-						newRecord.schema = request.response[index]["@schemaLocation"];
-					}
-					if(request.response[index].lifecycleState) {
-						newRecord.status = request.response[index].lifecycleState;
-					}
-					if(request.response[index].validFor) {
-						if(request.response[index].validFor.startDateTime) {
-							newRecord.start = request.response[index].validFor.startDateTime;
-						}
-						if(request.response[index].validFor.endDateTime) {
-							newRecord.end = request.response[index].validFor.endDateTime;
-						}
-					}
-					if(request.response[index].lastUpdate) {
-						newRecord.lastModified = request.response[index].lastUpdate;
-					}
 					var resChar = request.response[index].resourceCharacteristic;
-					for(var index1 in resChar) {
-						if(resChar[index1].value != []) {
-							var ValueArray = new Array();
-							ValueArray.push(resChar[index1].value);
-							for(var str in ValueArray) {
-								var str1 = JSON.stringify(ValueArray[str]);
-								var str2 = str1.trim();
-								var res = str2.replace(/"|{|[|[|}|]|]/g, " ");
-								resChar[index1].value = res;
-								newRecord.resourceChar = resChar;
-							}
-						} else {
-							newRecord.resourceChar = resChar;
+					var tabObj = new Object();
+					for (var indexRes in resChar) {
+						if(resChar[indexRes].name == "name") {
+							tabObj.name = resChar[indexRes].value.value;
 						}
+						if(resChar[indexRes].name == "qosInformation") {
+							var qos1 = resChar[indexRes].value.value.maxRequestedBandwidthDL;
+							var qos2 = resChar[indexRes].value.value.maxRequestedBandwidthUL;
+							var qos3 = resChar[indexRes].value.value.qosClassIdentifier;
+							tabObj.qos = "class:" + qos3 + ", UL:" + qos2 + ", DL:" + qos1;
+						}
+						if(resChar[indexRes].name == "chargingRule") {
+							tabObj.chargingRule = resChar[indexRes].value.value;
+						}
+						if(resChar[indexRes].name == "flowInformation") {
+							for(var indexFl in resChar[indexRes].value.value){
+								if(resChar[indexRes].value.value[indexFl].name == "flowInformationUp1") {
+									tabObj.flowUp = resChar[indexRes].value.value[indexFl].flowDirection;
+								}
+								if(resChar[indexRes].value.value[indexFl].name == "flowInformationDown1"){
+									tabObj.flowDown = resChar[indexRes].value.value[indexFl].flowDirection;
+								}
+							}
+						}
+						if(resChar[indexRes].name == "precedence") {
+							tabObj.precedence = resChar[indexRes].value.value;
+						}
+						if(request.response[index].resourceRelationship) {
+							tabObj.resourceRelationship = request.response[index].resourceRelationship;
+						}
+						if(request.response[index].resourceSpecification) {
+							tabObj.resourceSpecification = request.response[index].resourceSpecification;
+						}
+					vaadinItems[index] = tabObj;
 					}
-					if(request.response[index].resourceRelationship) {
-						newRecord.resourceRelationship = request.response[index].resourceRelationship;
-					}
-					if(request.response[index].resourceSpecification) {
-						newRecord.resourceSpecification = request.response[index].resourceSpecification;
-					}
-					vaadinItems[index] = newRecord;
 				}
 				callback(vaadinItems);
 			} else {
