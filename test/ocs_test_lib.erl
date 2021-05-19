1c72d9ae2f56c64a3f70a3e85303453d3251dd52
refath <refath@sigscale.com>
2021-05-19 20:08:23 +0000

    add nrf ref to table list

--- test/ocs_test_lib.erl
+++ test/ocs_test_lib.erl
@@ -26,7 +26,7 @@ initialize_db() ->
 					initialize_db()
 			end;
 		yes ->
-			Tables = [httpd_group, httpd_user, session, bucket, offer,
+			Tables = [httpd_group, httpd_user, nrf_ref, session, bucket, offer,
 					resource, product, service, client],
 			case mnesia:wait_for_tables([client, service, offer], 1000) of
 				{timeout, _} ->
