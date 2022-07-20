sigscale/ocs

commit 3b9792abf48a77501c6692b36b18853c14b3b68d

whitespace

src/ocs_radius_disconnect_fsm.erl
@@ -80,7 +80,7 @@
 %% @see //stdlib/gen_fsm:init/1
 %% @private
 %%
-init([Subscriber, {_, SessionAttributes}]) ->
+init([Id, Subscriber, {_, SessionAttributes}]) ->
 	process_flag(trap_exit, true),
 	NasIp = proplists:get_value(?NasIpAddress, SessionAttributes),
 	NasId = proplists:get_value(?NasIdentifier, SessionAttributes),
@@ -135,7 +135,7 @@ send_request(timeout, #statedata{nas_ip = Address, port = Port,
 	DisconnectRequest = radius:codec(DisconRec),
 	case gen_udp:open(0, [{active, once}, binary]) of
 		{ok, Socket} ->
-			case gen_udp:send(Socket, Address, Port, DisconnectRequest)of
+			case gen_udp:send(Socket, Address, Port, DisconnectRequest) of
 				ok ->
 					NewStateData = StateData#statedata{id = Id, socket = Socket,
 							request = DisconnectRequest},
@@ -146,7 +146,6 @@ send_request(timeout, #statedata{nas_ip = Address, port = Port,
 		{error, _Reason} ->
 				{next_state, send_request, StateData, ?TIMEOUT}
 	end.
-
 
 -spec receive_response(Event, StateData) -> Result
 	when

commit c4290963234c9f77aae85a4478fe8a1951b32ab3

correct error introduced in commit 3b9792abf

src/ocs_radius_disconnect_fsm.erl
@@ -80,7 +80,7 @@
 %% @see //stdlib/gen_fsm:init/1
 %% @private
 %%
-init([Id, Subscriber, {_, SessionAttributes}]) ->
+init([Subscriber, {_, SessionAttributes}]) ->
 	process_flag(trap_exit, true),
 	NasIp = proplists:get_value(?NasIpAddress, SessionAttributes),
 	NasId = proplists:get_value(?NasIdentifier, SessionAttributes),
@@ -88,7 +88,7 @@ init([Id, Subscriber, {_, SessionAttributes}]) ->
 	Id = 1,
 	case lookup_client(NasIp, NasId) of
 		{ok, #client{port = 0}} ->
-			{stop, {shutdown, disconnect_not_supported}};
+			ignore;
 		{ok, #client{address = Address, identifier = NasID,
 				secret = Secret, port = Port}} ->
 			StateData = #statedata{nas_ip = Address, 