d3c3fe7fb6fecc233b2ed5dc4843d95b5da5029a
Krishmanl <krishmanl@sigscale.com>
2023-04-21 05:51:03 +0000

    remove unwanted imports

--- priv/www/src/sig-tariff-table-list.js
+++ priv/www/src/sig-tariff-table-list.js
@@ -15,9 +15,6 @@ import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import '@polymer/iron-ajax/iron-ajax.js';
 import '@polymer/paper-fab/paper-fab.js';
 import '@polymer/iron-icons/iron-icons.js';
-import '@vaadin/vaadin-grid/theme/material/vaadin-grid.js';
-import '@vaadin/vaadin-grid/vaadin-grid.js';
-import '@vaadin/vaadin-grid/vaadin-grid-filter.js';
 import './style-element.js'
 
 class tableList extends PolymerElement {
