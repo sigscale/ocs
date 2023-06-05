749a823b2b245a7fa2e9399b7d08344d6ae2918a
Vance Shipley <vances@sigscale.org>
2023-06-05 11:37:56 +0800

    json patch content not accepted on hub operations

--- src/ocs_rest_res_hub_service.erl
+++ src/ocs_rest_res_hub_service.erl
@@ -36,8 +36,7 @@
 		ContentTypes :: list().
 %% @doc Provides list of resource representations accepted.
 content_types_accepted() ->
-	["application/json", "application/merge-patch+json",
-	"application/json-patch+json"].
+	["application/json".
 
 -spec content_types_provided() -> ContentTypes
 	when
