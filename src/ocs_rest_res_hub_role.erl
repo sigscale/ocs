c4972d1c527d773e3ab29c6ec807eccd33a545a1
Vance Shipley <vances@sigscale.org>
2023-06-05 11:43:49 +0800

    rfc7807 content provided for hub rest operations

--- src/ocs_rest_res_hub_role.erl
+++ src/ocs_rest_res_hub_role.erl
@@ -43,7 +43,7 @@ content_types_accepted() ->
 		ContentTypes :: list().
 %% @doc Provides list of resource representations available.
 content_types_provided() ->
-	["application/json"].
+	["application/json", "application/problem+json"].
 
 -spec post_hub(ReqBody) -> Result
 	when
