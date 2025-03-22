a78293405fc5b69a7d37724dffbb6f04800c599c
Vance Shipley <vances@sigscale.org>
2025-03-22 18:29:26 +0800

    Revert "opionally disable rating group independent credit control"
    This reverts commit fc54964bff4431663a1d9afe1e6c68929d568e61.

--- src/ocs_rest_res_nrf.erl
+++ src/ocs_rest_res_nrf.erl
@@ -719,30 +719,19 @@ rate2(RatingDataRef, Flag, SubscriptionIds,
 			{error, Reason}
 	end;
 rate2(_, _, _, [], Acc) when length(Acc) > 0 ->
-	{ok, RgIndependent} = application:get_env(ocs, nrf_rg_independent),
 	F = fun(#{"resultCode" := "SUCCESS"}) ->
 				true;
 			(_) ->
 				false
 	end,
-	rate3(RgIndependent, F, Acc);
-rate2(_, _, _, [], Acc) ->
-	{ok, Acc}.
-%% @hidden
-rate3(true = _RgIndependent, F, Acc) ->
 	case lists:any(F, Acc) of
 		true ->
 			{ok, Acc};
 		false ->
 			{error, out_of_credit}
 	end;
-rate3(false = _RgIndependent, F, Acc) ->
-	case lists:all(F, Acc) of
-		true ->
-			{ok, Acc};
-		false ->
-			{error, out_of_credit}
-	end.
+rate2(_, _, _, [], Acc) ->
+	{ok, Acc}.
 
 -spec nrf(Nrf) -> Nrf
 	when
